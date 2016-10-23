2187eb73f09f67ddd4988436363daa3719689994
Mindgamesnl <matsmoolhuizen@gmail.com>
2016-10-23 15:40:01 +0200

    Update config.php

--- WebInterface/config.php
+++ WebInterface/config.php
@@ -3,10 +3,7 @@
      *  OpenAudioMC
      *  Author: Mindgamesnl 
      */
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
     $wtitle = ""; // WebInterface title
     $sport  = "localhost:1185"; // OpenAudioMC Listen host and port (see config.yml)
     
