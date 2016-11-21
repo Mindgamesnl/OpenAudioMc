ea8108451a4d7550083fb9c948df203839d58342
Mindgamesnl <matsmoolhuizen@gmail.com>
2016-11-21 11:03:08 +0100

    OpenAudioMc.net WEB DEMO
    Test files for the playlist generator and main page

---
+++ OpenAudioMc.net/OpenAudioMc.net/index.php
@@ -0,0 +1,35 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+    	<title>OpenaudioMc | Portal</title>
+    	<link rel="stylesheet" href="css/custom.css" />
+    	<link rel="stylesheet" href="css/animate.css" />
+    	<link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Montserrat' />
+    	<link rel="icon" href="img/favicon.png" />
+    	<link rel="image" href="img/favicon.png" />
+    </head>
+    <body>
+    	<div class="header animated bounceInDown">
+    		<img class="logo animated pulse infinite" src="img/logo.png" />
+    	</div>
+    	<div class="content animated bounceInUp">
+    		<div class="server">
+    			<span class="montserrat">OpenaudioMc is the free and open source audio server!</span>
+    		</div>
+    		<div class="navbar" style="align-content: center;">
+    			<div class="iconCon">
+    				<a href="https://www.spigotmc.org/resources/openaudiomc.30691/">
+    					<img class="icon" src="img/spigot.png" />
+    					<span class="montserrat">OpenAudioMc on spigot</span>
+    				</a>
+    			</div>
+    			<div class="iconCon">
+    				<a href="Generator/Playlist/">
+    					<img class="icon" src="img/playlist.png" />
+    					<span class="montserrat">Playlist generator</span>
+    				</a>
+    			</div>
+    		</div>
+    	</div>
+    </body>
+    </html>
