0f418953c5f44db2275630c01bb560433fd34d83
MainPc (Home Setup) <matsmoolhuizen@gmail.com>
2016-12-16 17:25:14 +0100

    1.5.3

--- Plugin (eclipse)/src/me/mindgamesnl/openaudiomc/players/Events.java
+++ Plugin (eclipse)/src/me/mindgamesnl/openaudiomc/players/Events.java
@@ -6,7 +6,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
 import me.mindgamesnl.openaudiomc.main.Main;
+import me.mindgamesnl.openaudiomc.websocket.WsSender;
 
 public class Events implements Listener{
 	
@@ -16,9 +20,31 @@ public class Events implements Listener{
 	
 	@EventHandler
 	  public void onPlayerJoin(PlayerJoinEvent event) {
+		
 		  Player p = event.getPlayer();
+		  Player player = event.getPlayer();
+		  
 		  me.mindgamesnl.openaudiomc.websocket.WsSender.Send_Ws_Packet_To_Client(p, "{\"command\":\"connect\"}");
+		  
+    	  if (me.mindgamesnl.openaudiomc.main.Main.getPL().getConfig().getString("config.startsound").equalsIgnoreCase("none") || me.mindgamesnl.openaudiomc.main.Main.getPL().getConfig().getString("config.startsound").equalsIgnoreCase("off")) {
+    	  } else {
+    		  WsSender.Send_Ws_Packet_To_Client(player, "{\"command\":\"play\",\"line\":\"play\",\"src\":\"" + me.mindgamesnl.openaudiomc.main.Main.getPL().getConfig().getString("config.startsound") + "\"}");
+    	  }
+         
+    	  if (me.mindgamesnl.openaudiomc.main.config.Config.sream_live == true) {
+    		  WsSender.Send_Ws_Packet_To_Client(player, "{\"command\":\"startlive\",\"line\":\"loop\",\"src\":\"" + me.mindgamesnl.openaudiomc.main.config.Config.stream_source + "\"}");
+    	  }
+            
+    	  if (me.mindgamesnl.openaudiomc.detectors.checkDependencies.dependenciesComplete == true) {
+    		  for(ProtectedRegion r : WGBukkit.getRegionManager(player.getWorld()).getApplicableRegions(player.getLocation())) {				
+    			  if (me.mindgamesnl.openaudiomc.main.Main.getPL().getConfig().getBoolean("region.isvalid." + r.getId()) == true) {
+    				  WsSender.Send_Ws_Packet_To_Client(player, "{\"command\":\"play\",\"line\":\"region\",\"src\":\"" + me.mindgamesnl.openaudiomc.regions.regionManager.getRegionFile(r.getId()) + "\"}");
+    			  }
+    		  }
+    	  }
 	    }
+	
+	
 	  @EventHandler
 	  public void onPlayerQuit(PlayerQuitEvent event) {
 		  Player p = event.getPlayer();
