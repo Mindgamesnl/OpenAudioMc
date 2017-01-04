1b0535008e43b2c05fdd0c475bac17c09a20a69b
Mindgamesnl <matsmoolhuizen@gmail.com>
2017-01-04 18:20:06 +0100

    Update openaudio.php

--- WebInterface/php/inc/openaudio.php
+++ WebInterface/php/inc/openaudio.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<!-- OpenAudioMC by Mindgamesnl -->
-	
+
 	<!-- Title -->
 	<title>OpenAudioMC</title>
-	
+
 	<!-- CSS -->
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
@@ -26,10 +27,12 @@
 	<link rel="image" href="Images/small_logo.png" />
 
 	<!-- JS -->
-	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	<script src="main/Soundmanager2.js"></script>
 	<script src="dist/sweetalert.min.js"></script>
+	<script src="dist/qrcode.js"></script>
+	<script src="dist/qrcode.min.js"></script>
 	<script type="text/javascript" src="//www.gstatic.com/cv/js/sender/v1/cast_sender.js"></script>
 	<script src="main/openaudiomc.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -54,8 +57,34 @@
 			googlecastmode = "<?= htmlspecialchars(strip_tags($googlecast), ENT_QUOTES, 'UTF-8'); ?>";
 		</script>
 </head>
+
 <body>
 	<!-- Loading Page -->
+	<div class="container">
+		<div class="row"> </div>
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12">
+
+				<div class="alert alert-info alert-labeled">
+					<button type="button" class="close" data-dismiss="alert">
+					<span aria-hidden="true">×</span><span class="sr-only">Close</span>
+				</button>
+					<div class="alert-labeled-row">
+						<span class="alert-label alert-label-left alert-labelled-cell">
+                        <i class="fa fa-warning" style="color: #fff;
+    text-shadow: 1px 1px 1px #ccc;
+    font-size: 1.5em;"></i>
+                    </span>
+						<p class="alert-body alert-body-right alert-labelled-cell">
+							Placeholder (woking on a new messaging system)
+						</p>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
 	<div class="loader" id="loading_screen">
 		<center>
 			<img src="Images/small_logo.png" height="auto" width="80%" style="max-width: 400px; max-height: 400px;" />
@@ -63,9 +92,11 @@
 				Loading the audio client...
 			</h1>
 		</center>
-	</div><!-- ../Loading Page -->
-	
+	</div>
+	<!-- ../Loading Page -->
+
 	<!-- Main Page -->
+
 	<div id="faders"></div>
 	<div class="middlePage">
 		<input type="range" min="0" id="fade_slider" value="10" max="100" style="display:none;" />
@@ -86,7 +117,8 @@
 					<div class="col-md-8" id="contentBoxMain" style="border-left:1px solid #ccc;height:160px;">
 						<form class="form-horizontal">
 							<fieldset>
-								<div id="cogparent"><img class="btn-clipboard2 streamingLogo" id="cast_logo" src="Images/google-cast-logo.png" onclick="startCasting();"><i style="position: absolute; top: 20px; right: 20px;" class="btn-clipboard fa fa-cog fa-2x" onmouseover="this.className='draai btn-clipboard fa fa-cog fa-2x';" onmouseout="this.className='btn-clipboard fa fa-cog fa-2x';" data-toggle="modal" data-target="#settings" aria-hidden="true"></i></div>
+								<div id="cogparent"><img class="btn-clipboard2 streamingLogo" id="cast_logo" style="position: absolute; top: 20px; right: 50px;" src="Images/google-cast-logo.png" onclick="startCasting();"><i style="position: absolute; top: 20px; right: 20px;" class="btn-clipboard fa fa-cog fa-2x"
+										onmouseover="this.className='draai btn-clipboard fa fa-cog fa-2x';" onmouseout="this.className='btn-clipboard fa fa-cog fa-2x';" data-toggle="modal" data-target="#settings" aria-hidden="true"></i></div>
 								<h3 id="status">Status: <font style="color:green;">Loading</font></h3>
 								<hr />
 								<div id="voltextparant">
@@ -102,7 +134,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 		<!-- Messenger Alert -->
 		<div id="MessageManager" onclick='document.getElementById("MessageManager").className = "animated bounceOutUp";'>
 			<div class="animated bounceInDown">
@@ -118,8 +150,9 @@
 					</div>
 				</div>
 			</div>
-		</div> <!-- ../Messenger Alert -->
-		
+		</div>
+		<!-- ../Messenger Alert -->
+
 		<!-- Live Stream Mode -->
 		<div id="LiveBox">
 			<div class="animated bounceInDown">
@@ -133,8 +166,10 @@
 					</center>
 				</div>
 			</div>
-		</div> <!-- ../Live Stream Mode -->
-	</div> <!-- ../Main Page -->
+		</div>
+		<!-- ../Live Stream Mode -->
+	</div>
+	<!-- ../Main Page -->
 
 	<!-- Popup Modal for Settings -->
 	<div id="settings" class="modal fade" role="dialog">
@@ -151,7 +186,7 @@
 					<input type="checkbox" name="EnableBrowserNotifications" id="EnableBrowserNotifications" onchange='document.cookie="browser_notifications="+this.checked;' /> Enable browser notifications.<br />
 					<input type="checkbox" name="EnableSoundFading" id="EnableSoundFading" onchange='document.cookie="sound_fading="+this.checked;' /> Enable sound fading when available.
 					<p style="display:inline;">May cause performance issues</p>
-					<hr /> 
+					<hr />
 					<div class="form-group">
 						<label for="sel1">Fading speed</label>
 						<select class="form-control" id="fadingpseed" onchange='document.cookie = "sound_fading_speed="+getFadingType();'>
@@ -159,6 +194,11 @@
 							<option id="fading_speed_normal">Normal</option>
 							<option id="fading_speed_fast">Fast</option>
   						</select>
+					</div>
+					<hr />
+					<div class="button raised blue" onclick="showqr();" data-dismiss="modal">
+						<div class="center" fit>Show QR code</div>
+						<paper-ripple fit></paper-ripple>
 					</div>
 					<hr />
 					<b>*TIP* Did you know that you can use <i>/volume [number]</i> to change the volume in the server?</b>
@@ -174,6 +214,8 @@
 				</div>
 			</div>
 		</div>
-	</div> <!-- ../Popup Modal for Settings -->
+	</div>
+	<!-- ../Popup Modal for Settings -->
 </body>
+
 </html>
