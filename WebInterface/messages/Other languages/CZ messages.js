927695e9cb10e80dd8360e8e060a763b8c00eb7d
Liturkey <lukaspanek5@gmail.com>
2017-01-25 15:48:34 +0100

    Created Czech messages

---
+++ WebInterface/messages/Other languages/CZ messages.js
@@ -0,0 +1,18 @@
+// Translated by Liturkey
+
+//create messages object
+var messages = {};
+
+//define variables
+messages.status_not_connected     = "Ahoj <strong>%username%</strong>!";
+messages.not_found                = "Stav: <font style='color:Red;'>Player not found</font>";
+messages.connecting               = "Stav: <font style='color:orange;'>Connecting</font>";
+messages.could_not_connect        = "Stav: <font style='color:Red;'>Nelze se připojit</font>";
+messages.connected                = "Stav: <font style='color:green;'>Připojeno</font>";
+messages.disconnected             = "Stav: <font style='color:Red;'>Odpojeno</font>";
+messages.volume_max               = '<small>Hlasitost:</small> 100%';
+messages.volume_min               = '<small>Hlasitost:</small> 0%';
+messages.volume_var               = '<small>Hlasitost:</small> {{VOLUME}}%';
+messages.message_header           = "%username% | OpenAudioMc";
+messages.connection_error_header  = "Připojení zraceno";
+messages.connection_error_content = "Promiň %username%.<br>Ztratili jsme spojení se serverem. Chceš se zkusit znovu připojit?";
