9e696aa62a13571a866d052964faf41230ff2187
Mindgamesnl <matsmoolhuizen@gmail.com>
2017-04-13 10:41:45 +0200

    [Not tested] FINNANAAAALY ASYNC THREAD!!! WHOOOWHAHAHAHAAOAOAOOA

--- Plugin source code/src/net/openaudiomc/utils/webUtils.java
+++ Plugin source code/src/net/openaudiomc/utils/webUtils.java
@@ -3,7 +3,15 @@ package net.openaudiomc.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import net.openaudiomc.minecraft.Main;
+import net.openaudiomc.utils.callback.Callback;
 
 public class webUtils {
 	
@@ -20,5 +28,38 @@ public class webUtils {
 	    in.close();
 		return str;
 	}
+	
+	public static void asyncHttpRequest(final String request, final Callback<String> callback)
+	{
+	    new BukkitRunnable()
+	    {
+	        @Override
+	        public void run()
+	        {
+	            try {
+					URL url;
+					url = new URL(request);
+				    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+				    String str;
+					while ((str = in.readLine()) != null) {
+						 final String string = str; 
+				    }
+				    in.close();
+				    final String string = str;
+				    
+				    new BukkitRunnable()
+		            {
+		                @Override
+		                public void run()
+		                {
+		                    callback.execute(string);
+		                }
+		            }.runTask(Main.getPL());
+				    
+				} catch (IOException e) {
+				}
+	        }
+	    }.runTaskAsynchronously(Main.getPL());
+	} 
 
 }
