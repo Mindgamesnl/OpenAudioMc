6df4a6fa124dd4fccf8782dfb2949c4ca3fcee19
Mindgamesnl (Mats moolhuizen) <matsmoolhuizen@gmail.com>
2017-04-23 21:52:11 +0200

    Working now! (testing)

--- Plugin source code/src/net/openaudiomc/objects/audioSpeaker.java
+++ Plugin source code/src/net/openaudiomc/objects/audioSpeaker.java
@@ -1,14 +1,33 @@
 package net.openaudiomc.objects;
 
+import org.bukkit.Location;
+
 /**
  * Created by mats on 23-4-2017.
  */
 public class audioSpeaker {
 	
+	String id;
+	Location location;
+	String soundid;
 	
-
-    public audioSpeaker(String src) {
+    public audioSpeaker(String id, Location loc, String sid) {
+    	this.id = id;
+    	this.location = loc;
+    	this.soundid = sid;
     	
+    }
+    
+    public Location getLoc() {
+    	return this.location;
+    }
+    
+    public String getSoundId() {
+    	return this.soundid;
+    }
+    
+    public String getId() {
+    	return this.id;
     }
 
 }
