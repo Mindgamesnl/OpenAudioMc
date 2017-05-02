ebb6f4eb3468743dc226ea520b9f5dac4d1198e3
Mindgamesnl <matsmoolhuizen@gmail.com>
2017-05-02 18:37:04 +0200

    NEW COMMAND (see description)
    + /(command to request url) <volume OR v> <numver>
    
    Example: /audio volume 50

--- Plugin source code/src/net/openaudiomc/commands/AudioCommands.java
+++ Plugin source code/src/net/openaudiomc/commands/AudioCommands.java
@@ -10,10 +10,20 @@ import net.openaudiomc.files.Messages;
 import net.openaudiomc.minecraft.Main;
 import net.openaudiomc.players.Sessions;
 import net.openaudiomc.socket.timeoutManager;
+import org.bukkit.entity.Player;
 
 public class AudioCommands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (timeoutManager.ioready) {
+
+			if (args.length == 2) {
+				if (args[0].equalsIgnoreCase("volume") || args[0].equalsIgnoreCase("v")) {
+					Player p = (Player) sender;
+					p.chat("/volume" + args[1]);
+					return true;
+				}
+			}
+
 			String url = Messages.getColor("website-url");
 			url = url.replace("%name%", sender.getName());
 			url = url.replace("%session%", Sessions.get(sender.getName()));
