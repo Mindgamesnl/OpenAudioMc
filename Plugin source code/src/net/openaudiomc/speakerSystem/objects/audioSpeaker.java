bdd13bbd1f5f36266dd657c1f4a39513f73f2f36
Mindgamesnl <matsmoolhuizen@gmail.com>
2017-05-19 11:38:49 +0200

    One step closer to working speakers.

--- Plugin source code/src/net/openaudiomc/speakerSystem/objects/audioSpeaker.java
+++ Plugin source code/src/net/openaudiomc/speakerSystem/objects/audioSpeaker.java
@@ -11,6 +11,8 @@ public class audioSpeaker {
 	String id;
 	Location location;
 	String soundid;
+	Integer volume = 100;
+	Integer radius = 9;
 	
     public audioSpeaker(String id, Location loc, String sid) {
     	System.out.println("New speaker. ID:"+sid + " BLOCK:"+loc.getBlock().getType());
@@ -19,7 +21,23 @@ public class audioSpeaker {
     	this.soundid = sid;
     	
     }
-    
+
+    public void setRadius(Integer nr) {
+        this.radius = nr;
+    }
+
+    public void setVolume(Integer nv) {
+        this.volume = nv;
+    }
+
+    public Integer getVolume() {
+        return this.volume;
+    }
+
+    public Integer getRadius() {
+        return this.radius;
+    }
+
     public Location getLoc() {
     	return this.location;
     }
