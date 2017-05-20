d41d52ffbd0638ba56a92419f9d4676d39afdfeb
Mindgamesnl (Mats moolhuizen) <matsmoolhuizen@gmail.com>
2017-05-20 19:18:38 +0200

    Speakers partially redone

---
+++ Plugin source code/src/net/openaudiomc/utils/selector.java
@@ -0,0 +1,45 @@
+package net.openaudiomc.utils;
+
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import net.openaudiomc.minecraft.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mats on 19-5-2017.
+ */
+public class selector {
+
+    public static ArrayList<Player> playerSelector(CommandSender sender, String query) {
+        ArrayList<Player> list = new ArrayList<Player>();
+
+        if (query.startsWith("permission:")) {
+            String permission = query.replace("permission:", "");
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                if (p.hasPermission(permission)) {
+                    list.add(p);
+                }
+            }
+        } else if (query.startsWith("region:")) {
+            String region = query.replace("region:", "");
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                for(ProtectedRegion r : WGBukkit.getRegionManager(p.getWorld()).getApplicableRegions(p.getLocation())) {
+                    if (region.equalsIgnoreCase(r.getId())) {
+                        list.add(p);
+                    }
+                }
+            }
+        } else if (query.toLowerCase().contains(":".toLowerCase())) {
+            sender.sendMessage(Main.prefix + "Invalid selector.");
+        } else {
+            list.add(Bukkit.getPlayer(query));
+        }
+
+        return list;
+    }
+
+}
