36e5bb2043e95c45e50ec07f05d7aff1b276578b
Mats <matsmoolhuizen@gmail.com>
2017-12-14 13:52:05 +0100

    am i cool yet?

--- Source code/src/main/java/net/openaudiomc/jclient/modules/commands/CommandsModule.java
+++ Source code/src/main/java/net/openaudiomc/jclient/modules/commands/CommandsModule.java
@@ -2,11 +2,15 @@ package net.openaudiomc.jclient.modules.commands;
 
 import net.openaudiomc.jclient.OpenAudioMc;
 import net.openaudiomc.jclient.modules.commands.commands.SuplyUrlCommand;
+import net.openaudiomc.jclient.modules.commands.listeners.CustomCommandListener;
 
 public class CommandsModule {
 
     public CommandsModule(OpenAudioMc plugin) {
         plugin.getCommand("audio").setExecutor(new SuplyUrlCommand());
+        plugin.getCommand("connect").setExecutor(new SuplyUrlCommand());
+        plugin.getCommand("sound").setExecutor(new SuplyUrlCommand());
+        plugin.getServer().getPluginManager().registerEvents(new CustomCommandListener(), plugin);
     }
 
 }
