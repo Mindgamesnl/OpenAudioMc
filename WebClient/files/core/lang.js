724f1dd9d9b6f66672cfd8b4a78848fb2807ed33
Mats <matsmoolhuizen@gmail.com>
2018-12-26 21:16:33 +0100

    Started clean, for 2019 rework.

--- WebClient/files/core/lang.js
+++
@@ -1,9 +0,0 @@
-var lang = {};
-
-lang.hue_please_click = "Please click your hue link button to connect, Then press the hue icon again.";
-lang.unable_to_connect = "Sorry! we weren't able to connect to your hue bridge.";
-lang.hue_already_connected = "you are already connected!";
-lang.connected_to_bridge = "Success! you are now connected to: '%name%'";
-lang.hue_connecting = "Connecting... this may tike a few seconds";
-lang.socket_did_not_do_its_thing = "life has its ups and downs, we could not connect or lost connection to the minecraft server! no need to panic, try again later.";
-lang.unable_to_grab_light_data = "Failed to grab light data. Try refreshing your browser."
