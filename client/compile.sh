6e81a022ea9ed539c6dd27e2f443b6b7a45e08a1
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-02-05 12:45:08 +0100

    Update compile.sh

--- client/compile.sh
+++ client/compile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 webpack --config webpack.config.js
 ./node_modules/.bin/babel --presets babel-preset-es2015-ie ./dist/OpenAudioMc.bundle.js -o ./dist/OpenAudioMc.bundle.compiled.js
-echo finishing setup
 rm -rf target
 mkdir target
 cp -R ./src/libs ./target/libs/
