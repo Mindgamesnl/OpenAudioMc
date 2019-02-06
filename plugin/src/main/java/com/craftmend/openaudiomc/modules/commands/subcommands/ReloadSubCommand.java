7744acff641bfebd3c644f4cfddf50935337f598
Mats Moolhuizen <matsmoolhuizen@gmail.com>
2019-02-06 09:10:44 +0100

    Added a config reload command

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/modules/commands/subcommands/ReloadSubCommand.java
@@ -0,0 +1,26 @@
+package com.craftmend.openaudiomc.modules.commands.subcommands;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.modules.commands.CommandModule;
+import com.craftmend.openaudiomc.modules.commands.interfaces.SubCommand;
+import com.craftmend.openaudiomc.modules.commands.objects.Argument;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ReloadSubCommand extends SubCommand {
+
+    public ReloadSubCommand() {
+        super("reload");
+        registerArguments(new Argument("", "Reloads the config.yml file"));
+    }
+
+    @Override
+    public void onExecute(CommandSender sender, String[] args) {
+        message(sender, "Reloading config...");
+        OpenAudioMc.getInstance().reloadConfig();
+        message(sender, "Reloaded config.");
+    }
+}
