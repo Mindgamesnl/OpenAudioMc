44bbea01a3efcddaab09e1e96f689dd9d5593449
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-06-04 10:26:50 +0200

    code cleanup
    fuck you

--- client/src/js/modules/TimeService.js
+++ client/src/js/modules/TimeService.js
@@ -6,33 +6,30 @@ export class TimeService {
     }
 
     sync(serverTime, serverLocale) {
-        //sever date and time
+        // sever date and time
         let serverDate = new Date(serverTime);
 
         //local date and time
         let localUnixDate = new Date().getTime();
 
-        //server locale is the time zone offset in hours, we want to take that in account
-        //we do this by also setting the offset to the local time/date
-        //the offset is always positive, this is handled by the server
+        // server locale is the time zone offset in hours, we want to take that in account
+        // we do this by also setting the offset to the local time/date
+        // the offset is always positive, this is handled by the server
         localUnixDate += (serverLocale * 60 * 60 * 1000);
 
         let localDate = new Date(localUnixDate);
 
-        //diff in milli seconds
+        // diff in milli seconds
         this.isServerAhead = (serverDate.getTime() > localDate.getTime());
 
-        //relative timing for later calculation
+        // relative timing for later calculation
         this.msOffset = (this.isServerAhead ? (serverDate.getTime() - localDate.getTime()) : (localDate.getTime() - serverDate.getTime()));
     }
 
     getPredictedTime() {
-        //calculate the time based on the offset and which is ahead
-        return this.isServerAhead
-            ?
-            new Date(new Date().getTime() + this.msOffset)
-            :
-            new Date(new Date().getTime() - this.msOffset);
+        // calculate the time based on the offset and which is ahead
+        let time = new Date().getTime();
+        return new Date(this.isServerAhead ? time + this.msOffset : time - this.msOffset);
     }
 
 }
