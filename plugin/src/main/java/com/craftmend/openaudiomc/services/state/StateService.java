a6ccd2a546514cb7450a9dde09dd617cbfc794de
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-06-04 19:35:51 +0200

    add debug state logging

--- plugin/src/main/java/com/craftmend/openaudiomc/services/state/StateService.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/services/state/StateService.java
@@ -1,5 +1,6 @@
 package com.craftmend.openaudiomc.services.state;
 
+import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.services.state.interfaces.State;
 import com.craftmend.openaudiomc.services.state.states.BootingState;
 import lombok.Getter;
@@ -7,12 +8,21 @@ import lombok.Getter;
 public class StateService {
 
     @Getter private State currentState = new BootingState();
+    private OpenAudioMc openAudioMc;
 
-    public StateService() {
-
+    public StateService(OpenAudioMc openAudioMc) {
+        this.openAudioMc = openAudioMc;
     }
 
     public void setState(State state) {
+        // check if state logging is enabled
+        Boolean logState = this.openAudioMc.getConfigurationModule().getDataConfig().getBoolean("debug.log-state-changes");
+        if (logState) System.out.println(OpenAudioMc.getLOG_PREFIX() + "Changing state from ["
+                + currentState.getClass().getSimpleName()
+                + "] to ["
+                + state.getClass().getSimpleName() + "]");
+
+        // update class
         this.currentState = state;
     }
 
