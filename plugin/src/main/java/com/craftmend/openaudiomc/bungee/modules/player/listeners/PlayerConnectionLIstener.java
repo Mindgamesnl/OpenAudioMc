323f19031f1ab46ff50c4ca2ae28f4abea6ce59a
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-07-22 18:56:16 +0200

    Setup players for bungee!

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/bungee/modules/player/listeners/PlayerConnectionLIstener.java
@@ -0,0 +1,21 @@
+package com.craftmend.openaudiomc.bungee.modules.player.listeners;
+
+import com.craftmend.openaudiomc.OpenAudioMcCore;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class PlayerConnectionLIstener implements Listener {
+
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event) {
+        OpenAudioMcCore.getInstance().getNetworkingService().register(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onLogout(PlayerDisconnectEvent event) {
+        OpenAudioMcCore.getInstance().getNetworkingService().remove(event.getPlayer().getUniqueId());
+    }
+
+}
