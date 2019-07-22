2fe2fcb8bc5e50ae5de8d5d8d2dabdfc5cf459cc
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-07-22 21:42:58 +0200

    Fix cross platform commands, some more

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/commands/adapters/BungeeCommandSenderAdapter.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/commands/adapters/BungeeCommandSenderAdapter.java
@@ -2,16 +2,21 @@ package com.craftmend.openaudiomc.generic.commands.adapters;
 
 import com.craftmend.openaudiomc.generic.commands.interfaces.GenericExecutor;
 import lombok.AllArgsConstructor;
-import org.bukkit.command.CommandSender;
+import net.md_5.bungee.api.CommandSender;
 
 @AllArgsConstructor
-public class SpigotCommandSenderAdapter implements GenericExecutor {
+public class BungeeCommandSenderAdapter implements GenericExecutor {
 
     private CommandSender commandSender;
 
     @Override
     public void sendMessage(String message) {
         commandSender.sendMessage(message);
+    }
+
+    @Override
+    public Boolean hasPermission(String permission) {
+        return commandSender.hasPermission(permission);
     }
 
     @Override
