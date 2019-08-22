dcedab15614aac078f1f186a324b89ec9cb11e92
Mats <matsmoolhuizen@gmail.com>
2019-08-22 22:26:19 +0200

    don't be trigger happy

--- client/src/js/modules/voice/performance/LossProcessor.js
+++ client/src/js/modules/voice/performance/LossProcessor.js
@@ -6,6 +6,8 @@ export class LossProcessor extends AbstractAudio {
         super();
 
         this.queueSize = 5120;
+        this.unstableSeconds = 0;
+        this.stableSeconds = 0;
 
         this.minimalQueueSize = this.queueSize;
 
@@ -26,9 +28,21 @@ export class LossProcessor extends AbstractAudio {
 
     handleMeasurement(measurement) {
         if (this.isAcceptable(measurement)) {
-            this.decreaseBufferSize();
+            this.unstableSeconds = 0;
+            // if it has been stable for 5 seconds or longer, try to decrease the buffer size slowly
+            if (this.stableSeconds >= 5) {
+                this.decreaseBufferSize();
+                // reset to 3 to retry in 4 seconds
+                this.stableSeconds = 3;
+            }
+            this.stableSeconds++;
         } else {
-            this.increaseBufferSize();
+            this.stableSeconds = 0;
+            // if it has been unstable for 5 seconds or longer, do something about it
+            if (this.unstableSeconds >= 5) {
+                this.increaseBufferSize();
+            }
+            this.unstableSeconds++;
         }
     }
 
