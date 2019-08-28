2c59b57c9dee00e090a02131842a37dafa2d1f2c
Mats <matsmoolhuizen@gmail.com>
2019-08-28 18:00:30 +0200

    Add google drive

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/media/middleware/GoogleDriveMiddleware.java
@@ -0,0 +1,15 @@
+package com.craftmend.openaudiomc.generic.media.middleware;
+
+import com.craftmend.openaudiomc.generic.media.interfaces.UrlMutation;
+
+public class GoogleDriveMiddleware implements UrlMutation {
+
+    @Override
+    public String onRequest(String original) {
+        // check if it's a valid google drive link with 6 /'s
+        String[] parts = original.split("/");
+        if (parts.length != 6) return original;
+        return "http://docs.google.com/uc?export=open&id=" + parts[4];
+    }
+
+}
