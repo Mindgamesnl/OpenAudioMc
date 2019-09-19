64dd6332b343b01c8b4d28e781332a31eef76fb9
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-09-19 15:41:46 +0200

    Fixes

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/SpeakersSubCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/SpeakersSubCommand.java
@@ -177,20 +177,4 @@ public class SpeakersSubCommand extends SubCommand {
         }
     }
 
-    private boolean isInteger(String s) {
-        return isInteger(s,10);
-    }
-
-    private boolean isInteger(String s, int radix) {
-        if(s.isEmpty()) return false;
-        for(int i = 0; i < s.length(); i++) {
-            if(i == 0 && s.charAt(i) == '-') {
-                if(s.length() == 1) return false;
-                else continue;
-            }
-            if(Character.digit(s.charAt(i),radix) < 0) return false;
-        }
-        return true;
-    }
-
 }
