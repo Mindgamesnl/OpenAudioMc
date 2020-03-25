98dd536407efddc6e413d64405b83d48d1aec521
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-03-25 21:20:47 +0100

    FUCK YOU PAST ME

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/proxy/ProxyModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/proxy/ProxyModule.java
@@ -1,15 +1,12 @@
 package com.craftmend.openaudiomc.spigot.modules.proxy;
 
-import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.generic.loggin.OpenAudioLogger;
 import com.craftmend.openaudiomc.spigot.modules.proxy.enums.ClientMode;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
-import java.time.Duration;
 
 public class ProxyModule {
 
@@ -19,9 +16,9 @@ public class ProxyModule {
 
         FileConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(new File("spigot.yml"));
 
-        Boolean bungeeMode = yamlConfiguration.getBoolean("settings.bungeecord");
+        boolean bungeeMode = yamlConfiguration.getBoolean("settings.bungeecord");
 
-        if (bungeeMode != null && bungeeMode) {
+        if (bungeeMode) {
             OpenAudioLogger.toConsole("Starting in bungee mode");
             mode = ClientMode.NODE;
         } else {
