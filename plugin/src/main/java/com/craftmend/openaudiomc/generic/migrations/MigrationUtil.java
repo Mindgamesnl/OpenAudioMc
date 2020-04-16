b38a16a5399ceae6fe81153a214bfa1ffd99435a
Mats <matsmoolhuizen@gmail.com>
2020-04-16 14:02:28 +0200

    migrate web client

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/migrations/MigrationUtil.java
@@ -0,0 +1,17 @@
+package com.craftmend.openaudiomc.generic.migrations;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
+
+public class MigrationUtil {
+
+    public static void handleMigrations(OpenAudioMc main) {
+
+        // old client
+        if (main.getOAConfiguration().getString(StorageKey.AUTH_PUBLIC_URL).contains("app.openaudiomc")) {
+            main.getOAConfiguration().setString(StorageKey.AUTH_PUBLIC_URL, "https://client.openaudiomc.net/?&data=");
+        }
+
+    }
+
+}
