2d66bd8eab5a4d30db3a7c268d048fbc6210d0b2
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-04-16 19:15:42 +0200

    back to laptop

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/plus/updates/PlayerStateUdatePayload.java
@@ -0,0 +1,15 @@
+package com.craftmend.openaudiomc.generic.plus.updates;
+
+import com.craftmend.openaudiomc.generic.plus.object.PlusPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class PlayerStateUdatePayload {
+
+    private List<PlusPlayer> plusPlayers = new ArrayList<>();
+    private List<UUID> offlinePlayers = new ArrayList<>();
+
+
+}
