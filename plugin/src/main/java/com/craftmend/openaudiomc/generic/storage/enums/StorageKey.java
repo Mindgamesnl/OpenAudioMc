6f2b43f6d0aa1d6728682191c2169dc7f43f6f78
Mats <matsmoolhuizen@gmail.com>
2020-04-17 23:21:45 +0200

    Propper storage migration

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/storage/enums/StorageKey.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/storage/enums/StorageKey.java
@@ -4,54 +4,56 @@ import lombok.Getter;
 
 public enum StorageKey {
 
-    MESSAGE_PROMPT_TO_CONNECT("messages.suggest-connection", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLICK_TO_CONNECT("messages.click-to-connect", StorageLocation.CONFIG_FILE),
-    MESSAGE_LINK_EXPIRED("messages.click-link-expired", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_CLOSED("messages.client-closed", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_OPENED("messages.client-opened", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_VOLUME_CHANGED("messages.client-volume-change", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_VOLUME_INVALID("messages.client-volume-invalid", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_NOT_CONNECTED("messages.client-not-connected", StorageLocation.CONFIG_FILE),
-    MESSAGE_CLIENT_ALREADY_CONNECTED("messages.client-already-connected", StorageLocation.CONFIG_FILE),
-    MESSAGE_CALL_RINGING("messages.call-ringing", StorageLocation.CONFIG_FILE),
-    MESSAGE_CALL_ENDED("messages.call-left", StorageLocation.CONFIG_FILE),
-    MESSAGE_API_BOOTING("messages.api-starting-up", StorageLocation.CONFIG_FILE),
+    MESSAGE_PROMPT_TO_CONNECT(false, "messages.suggest-connection", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLICK_TO_CONNECT(false, "messages.click-to-connect", StorageLocation.CONFIG_FILE),
+    MESSAGE_LINK_EXPIRED(false, "messages.click-link-expired", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_CLOSED(false, "messages.client-closed", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_OPENED(false, "messages.client-opened", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_VOLUME_CHANGED(false, "messages.client-volume-change", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_VOLUME_INVALID(false, "messages.client-volume-invalid", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_NOT_CONNECTED(false, "messages.client-not-connected", StorageLocation.CONFIG_FILE),
+    MESSAGE_CLIENT_ALREADY_CONNECTED(false, "messages.client-already-connected", StorageLocation.CONFIG_FILE),
+    MESSAGE_CALL_RINGING(false, "messages.call-ringing", StorageLocation.CONFIG_FILE),
+    MESSAGE_CALL_ENDED(false, "messages.call-left", StorageLocation.CONFIG_FILE),
+    MESSAGE_API_BOOTING(false, "messages.api-starting-up", StorageLocation.CONFIG_FILE),
 
-    @Deprecated SETTING_CLIENT_TITLE("client.title", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTING_CLIENT_BACKGROUND("client.background", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTING_CLIENT_WELCOME_TEXT("client.welcome-message", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTING_CLIENT_ERROR_TEXT("client.error-message", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTINGS_HUE_CONNECTED_TEXT("client.hue-connected", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTINGS_HUE_CONNECTING_TEXT("client.hue-linking", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTINGS_HUE_AVAILABLE_TEXT("client.hue-bridge-found", StorageLocation.CONFIG_FILE),
-    @Deprecated SETTINGS_CLIENT_START_SOUND("client.start-sound", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTING_CLIENT_TITLE(true, "client.title", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTING_CLIENT_BACKGROUND(true, "client.background", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTING_CLIENT_WELCOME_TEXT(true, "client.welcome-message", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTING_CLIENT_ERROR_TEXT(true, "client.error-message", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTINGS_HUE_CONNECTED_TEXT(true, "client.hue-connected", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTINGS_HUE_CONNECTING_TEXT(true, "client.hue-linking", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTINGS_HUE_AVAILABLE_TEXT(true, "client.hue-bridge-found", StorageLocation.CONFIG_FILE),
+    @Deprecated SETTINGS_CLIENT_START_SOUND(true, "client.start-sound", StorageLocation.CONFIG_FILE),
 
-    SETTINGS_REMIND_TO_CONNECT("options.remind-to-connect", StorageLocation.CONFIG_FILE),
-    SETTINGS_REMIND_TO_CONNECT_INTERVAL("options.remind-to-connect-interval", StorageLocation.CONFIG_FILE),
-    SETTINGS_REGIONS_SYNC("options.sync-regions", StorageLocation.CONFIG_FILE),
-    SETTINGS_SPEAKER_SYNC("options.sync-speakers", StorageLocation.CONFIG_FILE),
-    SETTINGS_SPEAKER_RANGE("options.speaker-radius", StorageLocation.CONFIG_FILE),
-    SETTINGS_SEND_URL_ON_JOIN("options.send-on-join", StorageLocation.CONFIG_FILE),
-    SETTINGS_USE_WG_PRIORITY("options.use-region-priority", StorageLocation.CONFIG_FILE),
-    SETTINGS_LOOP_TEMP_REGIONS("options.loop-temp-regions", StorageLocation.CONFIG_FILE),
+    SETTINGS_REMIND_TO_CONNECT(false, "options.remind-to-connect", StorageLocation.CONFIG_FILE),
+    SETTINGS_REMIND_TO_CONNECT_INTERVAL(false, "options.remind-to-connect-interval", StorageLocation.CONFIG_FILE),
+    SETTINGS_REGIONS_SYNC(false, "options.sync-regions", StorageLocation.CONFIG_FILE),
+    SETTINGS_SPEAKER_SYNC(false, "options.sync-speakers", StorageLocation.CONFIG_FILE),
+    SETTINGS_SPEAKER_RANGE(false, "options.speaker-radius", StorageLocation.CONFIG_FILE),
+    SETTINGS_SEND_URL_ON_JOIN(false, "options.send-on-join", StorageLocation.CONFIG_FILE),
+    SETTINGS_USE_WG_PRIORITY(false, "options.use-region-priority", StorageLocation.CONFIG_FILE),
+    SETTINGS_LOOP_TEMP_REGIONS(false, "options.loop-temp-regions", StorageLocation.CONFIG_FILE),
 
-    DEBUG_LOG_STATE_CHANGES("debug.log-state-changes", StorageLocation.DATA_FILE),
+    DEBUG_LOG_STATE_CHANGES(false, "debug.log-state-changes", StorageLocation.DATA_FILE),
 
-    AUTH_PRIVATE_KEY("keyset.private", StorageLocation.DATA_FILE),
-    AUTH_PUBLIC_KEY("keyset.public", StorageLocation.DATA_FILE),
-    AUTH_KEY_VERSION("keyset.key-version", StorageLocation.DATA_FILE),
+    AUTH_PRIVATE_KEY(false, "keyset.private", StorageLocation.DATA_FILE),
+    AUTH_PUBLIC_KEY(false, "keyset.public", StorageLocation.DATA_FILE),
+    AUTH_KEY_VERSION(false, "keyset.key-version", StorageLocation.DATA_FILE),
 
-    REDIS_ENABLED("redis.enabled", StorageLocation.CONFIG_FILE),
-    REDIS_HOST("redis.host", StorageLocation.CONFIG_FILE),
-    REDIS_PORT("redis.port", StorageLocation.CONFIG_FILE),
-    REDIS_PASSWORD("redis.password", StorageLocation.CONFIG_FILE),
-    REDIS_USE_SSL("redis.useSSL", StorageLocation.CONFIG_FILE),
-    REDIS_SECTION("redis.section", StorageLocation.CONFIG_FILE),
+    REDIS_ENABLED(false, "redis.enabled", StorageLocation.CONFIG_FILE),
+    REDIS_HOST(false, "redis.host", StorageLocation.CONFIG_FILE),
+    REDIS_PORT(false, "redis.port", StorageLocation.CONFIG_FILE),
+    REDIS_PASSWORD(false, "redis.password", StorageLocation.CONFIG_FILE),
+    REDIS_USE_SSL(false, "redis.useSSL", StorageLocation.CONFIG_FILE),
+    REDIS_SECTION(false, "redis.section", StorageLocation.CONFIG_FILE),
     ;
 
     @Getter private String path;
     @Getter private StorageLocation storageLocation;
-    StorageKey(String path, StorageLocation storageLocation) {
+    @Getter private boolean isDeprecated;
+    StorageKey(boolean isDeprecated, String path, StorageLocation storageLocation) {
+        this.isDeprecated = isDeprecated;
         this.path = path;
         this.storageLocation = storageLocation;
     }
