e34cfc85b1918a886e2481ab43d47d9fa8430d92
Mats <matsmoolhuizen@gmail.com>
2020-04-23 18:31:13 +0200

    rest your mind

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/rest/endpoints/RestEndpoint.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/rest/endpoints/RestEndpoint.java
@@ -4,23 +4,27 @@ import com.craftmend.openaudiomc.OpenAudioMc;
 
 public enum RestEndpoint {
 
-    ENDPOINT_REGISTER("/api/v1/servers/register"),
-    ENDPOINT_LOGIN("/api/v1/servers/login/{private_key}"),
-    ENDPOINT_LOGOUT("/api/v1/servers/logout/{private_key}"),
+    ENDPOINT_REGISTER("/api/v1/servers/register", false),
+    ENDPOINT_LOGIN("/api/v1/servers/login/{private_key}", true),
+    ENDPOINT_LOGOUT("/api/v1/servers/logout/{private_key}", true),
 
-    ENDPOINT_PLUS_GENTOKEN("/api/v1/servers/createlogin"), // POST
-    ENDPOINT_PLUS_GET_SETTINGS("/api/v1/public/settings/{public_key}"),
-    ENDPOINT_PLUS_SAVE_SETTINGS("/api/v1/plus/settings"), // POST
-    ENDPOINT_PLUS_UPDATE_PLAYERS("/api/v1/plus/players"); // ALSO POST
+    ENDPOINT_PLUS_GENTOKEN("/api/v1/servers/createlogin", false), // POST
+    ENDPOINT_PLUS_GET_SETTINGS("/api/v1/public/settings/{public_key}", true),
+    ENDPOINT_PLUS_SAVE_SETTINGS("/api/v1/plus/settings", false), // POST
+    ENDPOINT_PLUS_UPDATE_PLAYERS("/api/v1/plus/players", false); // ALSO POST
 
     private String url;
-    RestEndpoint(String url) {
+    private boolean translate;
+    RestEndpoint(String url, boolean translate) {
         this.url = url;
+        this.translate = true;
     }
 
     public String getURL() {
-        this.url = this.url.replace("{private_key}", OpenAudioMc.getInstance().getAuthenticationService().getServerKeySet().getPrivateKey().getValue());
-        this.url = this.url.replace("{public_key}", OpenAudioMc.getInstance().getAuthenticationService().getServerKeySet().getPublicKey().getValue());
+        if (this.translate) {
+            this.url = this.url.replace("{private_key}", OpenAudioMc.getInstance().getAuthenticationService().getServerKeySet().getPrivateKey().getValue());
+            this.url = this.url.replace("{public_key}", OpenAudioMc.getInstance().getAuthenticationService().getServerKeySet().getPublicKey().getValue());
+        }
         return this.url;
     }
 }
