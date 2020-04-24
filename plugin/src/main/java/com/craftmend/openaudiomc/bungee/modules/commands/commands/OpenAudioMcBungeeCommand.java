10239c944afeae454545d99b8a2df3e885a59569
Mats <matsmoolhuizen@gmail.com>
2020-04-24 10:03:07 +0200

    Catch crash in middleware instead of duplicating it so much

--- plugin/src/main/java/com/craftmend/openaudiomc/bungee/modules/commands/commands/OpenAudioMcBungeeCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/bungee/modules/commands/commands/OpenAudioMcBungeeCommand.java
@@ -8,6 +8,7 @@ import com.craftmend.openaudiomc.generic.commands.helpers.CommandMiddewareExecut
 import com.craftmend.openaudiomc.generic.commands.interfaces.CommandMiddleware;
 import com.craftmend.openaudiomc.generic.commands.interfaces.GenericExecutor;
 import com.craftmend.openaudiomc.generic.commands.interfaces.SubCommand;
+import com.craftmend.openaudiomc.generic.commands.middleware.CatchCrashMiddleware;
 import com.craftmend.openaudiomc.generic.commands.middleware.CleanStateCheckMiddleware;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
@@ -16,6 +17,7 @@ public class OpenAudioMcBungeeCommand extends Command {
 
     private CommandModule commandModule = OpenAudioMc.getInstance().getCommandModule();
     private CommandMiddleware[] commandMiddleware = new CommandMiddleware[] {
+            new CatchCrashMiddleware(),
             new CleanStateCheckMiddleware()
     };
 
@@ -26,10 +28,6 @@ public class OpenAudioMcBungeeCommand extends Command {
     @Override
     public void execute(CommandSender originalSender, String[] args) {
         GenericExecutor sender = new BungeeCommandSenderAdapter(originalSender);
-        if (!OpenAudioMc.getInstance().getAuthenticationService().isSuccesfull()) {
-            sender.sendMessage(commandModule.getCommandPrefix() + OpenAudioMc.getInstance().getAuthenticationService().getFailureMessage());
-            return;
-        }
 
         if (args.length == 0) {
             sender.sendMessage(commandModule.getCommandPrefix() + "OpenAudioMc version " + OpenAudioMcBungee.getInstance().getDescription().getVersion() + ". For help, please use /openaudio help");
