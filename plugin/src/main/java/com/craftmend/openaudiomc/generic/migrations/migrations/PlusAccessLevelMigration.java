3fc95200e813017f8f63a25c6dbdc46250bf6f5c
Mats <matsmoolhuizen@gmail.com>
2020-04-26 01:45:35 +0200

    cleanup and clear cache

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/migrations/migrations/PlusAccessLevelMigration.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/migrations/migrations/PlusAccessLevelMigration.java
@@ -9,7 +9,7 @@ import com.craftmend.openaudiomc.generic.core.storage.enums.StorageKey;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PlusAccessLevelMigration implements SimpleMigration {
+public class PlusAccessLevelMigration extends SimpleMigration {
 
     @Override
     public boolean shouldBeRun() {
@@ -19,33 +19,6 @@ public class PlusAccessLevelMigration implements SimpleMigration {
 
     @Override
     public void execute() {
-        ConfigurationImplementation config = OpenAudioMc.getInstance().getConfigurationImplementation();
-
-        // settings that should be moved over
-        Map<StorageKey, Object> oldValues = new HashMap<>();
-        for (StorageKey value : StorageKey.values()) {
-            if (!value.isDeprecated()) {
-                oldValues.put(value, config.get(value));
-            }
-        }
-
-        // force reload conf
-        config.saveAllhard();
-        config.reloadConfig();
-
-        // force update values
-        for (Map.Entry<StorageKey, Object> entry : oldValues.entrySet()) {
-            StorageKey key = entry.getKey();
-            Object value = entry.getValue();
-            if (value == null) {
-                OpenAudioLogger.toConsole("Skipping migration key " + key.name() + " because its null.");
-            } else {
-                OpenAudioLogger.toConsole("Migrating " + key.name() + " value " + value.toString());
-            }
-            config.set(key, value);
-        }
-
-        // soft save to reflect the old values and write them to the new file
-        config.saveAll();
+        migrateFilesFromResources();
     }
 }
