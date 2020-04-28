c5de81633e20544776fe30d5ec3ba19c6a9a281d
Mats <matsmoolhuizen@gmail.com>
2020-04-28 19:15:52 +0200

    Finish traincarts media

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/traincarts/TrainCartsModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/traincarts/TrainCartsModule.java
@@ -1,30 +1,61 @@
 package com.craftmend.openaudiomc.spigot.modules.traincarts;
 
+import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.networking.packets.PacketClientDestroyMedia;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
+import com.craftmend.openaudiomc.spigot.modules.players.objects.SpigotConnection;
 import com.craftmend.openaudiomc.spigot.modules.traincarts.listeners.TrainListener;
-import com.craftmend.openaudiomc.spigot.modules.traincarts.models.CurrentTrainMedia;
-import com.craftmend.openaudiomc.spigot.modules.traincarts.signs.OpenAudioSign;
+import com.craftmend.openaudiomc.spigot.modules.traincarts.models.TrainMedia;
+import com.craftmend.openaudiomc.spigot.modules.traincarts.signs.AudioSign;
 import lombok.Getter;
+import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class TrainCartsModule {
 
-    @Getter private Map<String, CurrentTrainMedia> trainMediaMap = new HashMap<>();
+    @Getter private Map<String, TrainMedia> trainMediaMap = new HashMap<>();
 
     public TrainCartsModule(OpenAudioMcSpigot openAudioMcSpigot) {
-        SignAction.register(new OpenAudioSign(this));
+        SignAction.register(new AudioSign(this));
         openAudioMcSpigot.getServer().getPluginManager().registerEvents(new TrainListener(this), openAudioMcSpigot);
     }
 
-    public CurrentTrainMedia getMediaFromTrain(String trainName) {
+    public TrainMedia getMediaFromTrain(String trainName) {
         return trainMediaMap.get(trainName);
     }
 
     public void handleTrainDeletion(String trainName) {
         trainMediaMap.remove(trainName);
+    }
+
+    public void stopStrain(String trainName, SignActionEvent event) {
+        TrainMedia media = getMediaFromTrain(trainName);
+        if (media == null) return;
+
+        if (!event.getGroup().isEmpty()) {
+            for (Player playerPassenger : event.getMember().getEntity().getPlayerPassengers()) {
+                SpigotConnection spigotConnection = OpenAudioMcSpigot.getInstance().getPlayerModule().getClient(playerPassenger);
+                OpenAudioMc.getInstance().getNetworkingService().send(spigotConnection.getClientConnection(), new PacketClientDestroyMedia(media.getMediaId().toString()));
+            }
+        }
+
+        trainMediaMap.remove(trainName);
+    }
+
+    public void registerTrain(String trainName, String source, SignActionEvent event) {
+        TrainMedia media = new TrainMedia(source);
+        trainMediaMap.put(trainName, media);
+
+        if (!event.getGroup().isEmpty()) {
+            for (Player playerPassenger : event.getMember().getEntity().getPlayerPassengers()) {
+                SpigotConnection spigotConnection = OpenAudioMcSpigot.getInstance().getPlayerModule().getClient(playerPassenger);
+                spigotConnection.getClientConnection().sendMedia(media.toMedia());
+            }
+        }
     }
 
 }
