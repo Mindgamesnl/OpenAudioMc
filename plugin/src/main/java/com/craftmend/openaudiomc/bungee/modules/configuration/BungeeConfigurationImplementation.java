77641b5102625a6ff661569c5603ebfa383e41b1
Mats Moolhuizen <mats@craftmend.com>
2020-05-01 09:52:45 +0200

    Don't touch the config file

--- plugin/src/main/java/com/craftmend/openaudiomc/bungee/modules/configuration/BungeeConfigurationImplementation.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/bungee/modules/configuration/BungeeConfigurationImplementation.java
@@ -238,7 +238,6 @@ public class BungeeConfigurationImplementation implements ConfigurationImplement
 
     @Override
     public void saveAllhard() {
-        saveAll();
         saveDefaultFile("config.yml", true);
     }
 
