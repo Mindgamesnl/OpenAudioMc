e23151ffdfd96302588f9593d14b5e9e33a67002
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-04 09:24:38 +0200

    Kill old values

--- client/src/js/modules/ui/Messages.js
+++ client/src/js/modules/ui/Messages.js
@@ -1,12 +1,13 @@
 export class Messages {
 
     constructor(main) {
+        this.fallback = "No message provided in oa+";
         this.main = main;
         this.hueConnected = "You are now connected with your Philips Hue Lights! Please select your group (you can always change this later) and click \"player\" in the left bottem corner to return to the home menu.";
         this.hueLinking = "Press the link button on your hue bridge within %sec% seconds to connect.";
         this.hueWelcome = "We've detected a Philips Hue Bridge! You can connect it to OpenAudioMc and link your in-game session to your lights. This allows your current server to add real-time lighting effects. You can unlink at any time and manually select your lights.";
-        this.welcomeMessage = "Welcome! You are <a style='color:green;'>connected</a> to the audio client!";
-        this.errorMessage = "Your session with the webclient terminated unexpectedly. Use /audio in-game to request a new url.";
+        this.welcomeMessage = this.fallback;
+        this.errorMessage = this.fallback;
     }
 
     apply() {
