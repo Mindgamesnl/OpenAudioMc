0abbd6ecb395eabfdaab80b4809e796c519309ed
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-18 00:07:30 +0200

    more weird stuff

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/SpeakerModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/SpeakerModule.java
@@ -182,7 +182,7 @@ public class SpeakerModule {
 
                 if (skull.getOwningPlayer() == null) return false;
                 if (skull.getOwningPlayer().getName() == null) return false;
-                return skull.getOwningPlayer().getName().equalsIgnoreCase("OpenAudioMc") || skull.getOwningPlayer().getUniqueId().toString().equals("c0db149e-d498-4a16-8e35-93d57577589f");
+                return skull.getOwningPlayer().getName().equalsIgnoreCase("OpenAudioMc") || skull.getOwningPlayer().getUniqueId().toString().equalsIgnoreCase("c0db149e-d498-4a16-8e35-93d57577589f");
             } else {
                 if (skull.getOwner() == null) return false;
                 return skull.getOwner().equalsIgnoreCase("OpenAudioMc");
