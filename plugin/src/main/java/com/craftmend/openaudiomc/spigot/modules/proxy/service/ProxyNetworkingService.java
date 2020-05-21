b3d6271c765f4fa23a3ac4a3b54847eb6c1bae9b
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-05-21 12:56:09 +0200

    Add event middleware

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/proxy/service/ProxyNetworkingService.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/proxy/service/ProxyNetworkingService.java
@@ -3,6 +3,7 @@ package com.craftmend.openaudiomc.spigot.modules.proxy.service;
 import com.craftmend.openaudiomc.generic.networking.NetworkingService;
 import com.craftmend.openaudiomc.generic.networking.abstracts.AbstractPacket;
 import com.craftmend.openaudiomc.generic.networking.client.objects.player.ClientConnection;
+import com.craftmend.openaudiomc.generic.networking.interfaces.INetworkingEvents;
 import com.craftmend.openaudiomc.generic.networking.interfaces.INetworkingService;
 import com.craftmend.openaudiomc.generic.node.packets.ForwardSocketPacket;
 import com.craftmend.openaudiomc.generic.player.SpigotPlayerAdapter;
@@ -11,18 +12,18 @@ import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
 import com.craftmend.openaudiomc.spigot.modules.proxy.listeners.BungeePacketListener;
 import com.ikeirnez.pluginmessageframework.PacketPlayer;
 import com.ikeirnez.pluginmessageframework.implementations.BukkitPacketManager;
+import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class ProxyNetworkingService extends INetworkingService {
 
+    @Getter private Set<INetworkingEvents> eventHandlers = new HashSet<>();
     private NetworkingService realService = new NetworkingService();
     private BukkitPacketManager packetManager;
 
@@ -41,6 +42,7 @@ public class ProxyNetworkingService extends INetworkingService {
         // handle packet if it should be passed to bungee
         // forward every packet starting with PacketClient
         if (packet.getClass().getSimpleName().startsWith("PacketClient")) {
+            for (INetworkingEvents event : getEvents()) event.onPacketSend(client, packet);
             packet.setClient(client.getPlayer().getUniqueId());
             Player player = ((SpigotPlayerAdapter) client.getPlayer()).getPlayer();
             packetManager.sendPacket(new PacketPlayer(player), new ForwardSocketPacket(packet));
@@ -85,5 +87,16 @@ public class ProxyNetworkingService extends INetworkingService {
     @Override
     public void requestRoomCreation(List<RoomMember> members, Consumer<Boolean> wasSucessful) {
         // unused in fake system
+    }
+
+
+    @Override
+    public Set<INetworkingEvents> getEvents() {
+        return eventHandlers;
+    }
+
+    @Override
+    public void addEventHandler(INetworkingEvents events) {
+        eventHandlers.add(events);
     }
 }
