63210582c4d77be7ed8f60d7e7fb61fa3fd85ecb
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-24 23:12:24 +0200

    Migration for the config field

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/migrations/migrations/TipsSettingMigration.java
@@ -0,0 +1,20 @@
+package com.craftmend.openaudiomc.generic.migrations.migrations;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.core.interfaces.ConfigurationImplementation;
+import com.craftmend.openaudiomc.generic.core.storage.enums.StorageKey;
+import com.craftmend.openaudiomc.generic.migrations.interfaces.SimpleMigration;
+
+public class TipsSettingMigration extends SimpleMigration {
+
+    @Override
+    public boolean shouldBeRun() {
+        ConfigurationImplementation config = OpenAudioMc.getInstance().getConfigurationImplementation();
+        return !config.hasStorageKey(StorageKey.SETTINGS_STAFF_TIPS);
+    }
+
+    @Override
+    public void execute() {
+        migrateFilesFromResources();
+    }
+}
