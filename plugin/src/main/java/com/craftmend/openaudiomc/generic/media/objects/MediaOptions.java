c206b60dfbeb1b0f4c07f7593a577ae7b90e960f
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-26 23:35:35 +0200

    Add validation and volume property

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/media/objects/MediaOptions.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/media/objects/MediaOptions.java
@@ -12,5 +12,22 @@ public class MediaOptions {
     private int expirationTimeout = -1;
     private boolean pickUp = true;
     private int fadeTime = 0;
+    private int volume = 100;
+
+    public OptionalError validate() {
+        if (volume > 100)
+            return new OptionalError(true, "Volume may not be over 100");
+
+        if (volume < 0)
+            return new OptionalError(true, "Volume may not be lower than 0");
+
+        if (volume == 0)
+            return new OptionalError(true, "You shouldn't even play it if the volume is 0");
+
+        if (fadeTime < 0)
+            return new OptionalError(true, "Fade time can't be negative");
+
+        return new OptionalError(false, "");
+    }
 
 }
