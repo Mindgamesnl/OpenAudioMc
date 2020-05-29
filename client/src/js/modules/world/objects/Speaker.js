c64179655068f7d67c115410a497b2c5a406de0b
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-29 10:09:35 +0200

    force packet order and make it nod ded lmao

--- client/src/js/modules/world/objects/Speaker.js
+++ client/src/js/modules/world/objects/Speaker.js
@@ -4,11 +4,6 @@ import {SPEAKER_2D, SPEAKER_3D} from "../constants/SpeakerType";
 export class Speaker {
 
     constructor(id, source, location, type, maxDistance) {
-        if (!(id instanceof String)) throw new Error("Argument 1 must be an id");
-        if (!(source instanceof String)) throw new Error("Argument 2 must be a source");
-        if (!(location instanceof Vector3)) throw new Error("Argument 3 must be a Vector");
-        if (!(type == SPEAKER_2D || type == SPEAKER_3D)) throw new Error("A speaker must be 2d or 3d");
-
         this.id = id;
         this.source = source;
         this.location = location;
