13776d6a34febec04c54349bd6b3f7504165a2e4
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-29 11:38:28 +0200

    change type in the GUI

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/menu/SpeakerMenu.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/menu/SpeakerMenu.java
@@ -4,6 +4,7 @@ import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.generic.core.interfaces.ConfigurationImplementation;
 import com.craftmend.openaudiomc.generic.core.storage.enums.StorageLocation;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
+import com.craftmend.openaudiomc.spigot.modules.speakers.enums.SpeakerType;
 import com.craftmend.openaudiomc.spigot.modules.speakers.objects.Speaker;
 import com.craftmend.openaudiomc.spigot.services.clicklib.Item;
 import com.craftmend.openaudiomc.spigot.services.clicklib.menu.Menu;
@@ -13,19 +14,64 @@ import org.bukkit.Material;
 public class SpeakerMenu extends Menu {
 
     public SpeakerMenu(Speaker speaker) {
-        super(ChatColor.BLUE + "Updating speaker", 9);
+        super(ChatColor.BLUE + "Updating speaker", 18);
 
-        setItem(0, new Item(OpenAudioMcSpigot.getInstance().getSpeakerModule().getSkull())
-                .setName(ChatColor.YELLOW + "Playing: " + ChatColor.AQUA + speaker.getMedia().getSource()));
+        // show source
+        setItem(3, new Item(OpenAudioMcSpigot.getInstance().getSpeakerModule().getSkull())
+                .setName(ChatColor.YELLOW + "Playing: " + ChatColor.AQUA + speaker.getMedia().getSource())
+                .setLore(new String[]{})
+        );
 
-        setItem(1, getDistanceItem(speaker, 2));
-        setItem(2, getDistanceItem(speaker, 4));
-        setItem(3, getDistanceItem(speaker, 5));
-        setItem(4, getDistanceItem(speaker, 8));
-        setItem(5, getDistanceItem(speaker, 10));
-        setItem(6, getDistanceItem(speaker, 12));
-        setItem(7, getDistanceItem(speaker, 14));
-        setItem(8, getDistanceItem(speaker, 16));
+        // toggle mode
+        setItem(5, getTypeButton(speaker));
+
+        // slider to change volume/range of the speaker
+        setItem(9, getDistanceItem(speaker, 2));
+        setItem(10, getDistanceItem(speaker, 4));
+        setItem(11, getDistanceItem(speaker, 5));
+        setItem(12, getDistanceItem(speaker, 8));
+        setItem(13, getDistanceItem(speaker, 10));
+        setItem(14, getDistanceItem(speaker, 12));
+        setItem(15, getDistanceItem(speaker, 14));
+        setItem(16, getDistanceItem(speaker, 16));
+        setItem(17, getDistanceItem(speaker, 18));
+    }
+
+    private Item getTypeButton(Speaker speaker) {
+        Item item = new Item(Material.COMPASS);
+        item.setName(ChatColor.AQUA + "Current mode: " + ChatColor.WHITE + speaker.getSpeakerType().getName());
+
+        // figure it out based on INDEX instead of FLIPS so we can re-use this menu
+        // if we decide to add a third mode
+        SpeakerType[] possibleModes = SpeakerType.values();
+
+        // ind out current index
+        int currentMode = 0;
+        for (SpeakerType possibleMode : possibleModes) {
+            if (possibleMode == speaker.getSpeakerType()) break;
+            currentMode++;
+        }
+
+        // wrap around if out of bounds
+        int nextMode = (currentMode + 1);
+        if (nextMode > (possibleModes.length - 1)) nextMode = 0;
+
+        SpeakerType nextSelectableMode = possibleModes[nextMode];
+
+        item.setLore(new String[] {
+                ChatColor.AQUA + speaker.getSpeakerType().getDescription(),
+                "",
+                ChatColor.AQUA + "Click to change to " + ChatColor.GRAY + nextSelectableMode.getName()
+        });
+
+        // makes me wonder why i thought it would be use full to get the clicked item, its just ugly code
+        // maybe remove in the future? don't think its used anywhere.
+        item.onClick((clicker, clickedItem) -> {
+            speaker.setSpeakerType(nextSelectableMode);
+            new SpeakerMenu(speaker).openFor(clicker);
+        });
+
+        return item;
     }
 
     private Item getDistanceItem(Speaker speaker, int distance) {
