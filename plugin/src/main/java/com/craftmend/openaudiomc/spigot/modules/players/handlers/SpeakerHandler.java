634a572c9a6fc887a2bf276fa7a61e11ae3db6d2
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-05-29 09:49:03 +0200

    implement basic packet stracture for creation of speakers

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/players/handlers/SpeakerHandler.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/players/handlers/SpeakerHandler.java
@@ -1,15 +1,21 @@
 package com.craftmend.openaudiomc.spigot.modules.players.handlers;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.networking.packets.client.speakers.PacketClientCreateSpeaker;
+import com.craftmend.openaudiomc.generic.networking.packets.client.speakers.PacketClientUpdateLocation;
+import com.craftmend.openaudiomc.generic.networking.payloads.out.speakers.ClientPlayerLocationPayload;
+import com.craftmend.openaudiomc.generic.networking.payloads.out.speakers.ClientSpeakerCreatePayload;
+import com.craftmend.openaudiomc.generic.networking.payloads.out.speakers.objects.ClientSpeaker;
+import com.craftmend.openaudiomc.generic.networking.payloads.out.speakers.objects.SpeakerType;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
 import com.craftmend.openaudiomc.generic.media.objects.MediaUpdate;
 import com.craftmend.openaudiomc.spigot.modules.players.interfaces.ITickableHandler;
 import com.craftmend.openaudiomc.spigot.modules.players.objects.SpigotConnection;
 import com.craftmend.openaudiomc.spigot.modules.speakers.objects.ApplicableSpeaker;
-import com.craftmend.openaudiomc.generic.networking.packets.client.media.PacketClientCreateMedia;
 import com.craftmend.openaudiomc.generic.networking.packets.client.media.PacketClientDestroyMedia;
 import com.craftmend.openaudiomc.generic.networking.packets.client.media.PacketClientUpdateMedia;
 import lombok.AllArgsConstructor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.math.BigDecimal;
@@ -28,22 +34,21 @@ public class SpeakerHandler implements ITickableHandler {
      */
     @Override
     public void tick() {
-        // send player location update
-
         List<ApplicableSpeaker> applicableSpeakers = new ArrayList<>(OpenAudioMcSpigot.getInstance().getSpeakerModule().getApplicableSpeakers(player.getLocation()));
-
         List<ApplicableSpeaker> enteredSpeakers = new ArrayList<>(applicableSpeakers);
         enteredSpeakers.removeIf(speaker -> containsSpeaker(spigotConnection.getSpeakers(), speaker));
-
         List<ApplicableSpeaker> leftSpeakers = new ArrayList<>(spigotConnection.getSpeakers());
         leftSpeakers.removeIf(speaker -> containsSpeaker(applicableSpeakers, speaker));
 
         enteredSpeakers.forEach(entered -> {
             if (!isPlayingSpeaker(entered)) {
-                OpenAudioMc.getInstance().getNetworkingService().send(spigotConnection.getClientConnection(), new PacketClientCreateMedia(entered.getSpeaker().getMedia(), entered.getDistance(), entered.getSpeaker().getRadius()));
+                OpenAudioMc.getInstance().getNetworkingService().send(spigotConnection.getClientConnection(),
+                        new PacketClientCreateSpeaker(new ClientSpeakerCreatePayload(toClientSpeaker(entered)))
+                );
             }
         });
 
+        // this can be removed, since we do this client side aye lmao, not my problem anymore
         spigotConnection.getSpeakers().forEach(current -> {
             if (containsSpeaker(applicableSpeakers, current)) {
                 ApplicableSpeaker selector = filterSpeaker(applicableSpeakers, current);
@@ -54,10 +59,12 @@ public class SpeakerHandler implements ITickableHandler {
             }
         });
 
+        // send deletion packets
         leftSpeakers.forEach(left -> OpenAudioMc.getInstance().getNetworkingService().send(spigotConnection.getClientConnection(), new PacketClientDestroyMedia(left.getSpeaker().getMedia().getMediaId())));
 
         spigotConnection.setCurrentSpeakers(applicableSpeakers);
 
+        // update location if the client is listening to something
         if (!applicableSpeakers.isEmpty()) {
             Location location = player.getLocation();
             ClientPlayerLocationPayload locationPayload = new ClientPlayerLocationPayload(
@@ -97,6 +104,22 @@ public class SpeakerHandler implements ITickableHandler {
         BigDecimal bd = BigDecimal.valueOf(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
         return bd.doubleValue();
+    }
+
+    private ClientSpeaker toClientSpeaker(ApplicableSpeaker speaker) {
+        String id;
+        if (speaker.getSpeakerType() == SpeakerType.SPEAKER_3D) {
+            id = speaker.getSpeaker().getId().toString();
+        } else {
+            id = speaker.getSpeaker().getMedia().getMediaId();
+        }
+
+        return new ClientSpeaker(
+                speaker.getLocation(),
+                speaker.getSpeakerType(),
+                id,
+                speaker.getSpeaker().getSource()
+        );
     }
 
 }
