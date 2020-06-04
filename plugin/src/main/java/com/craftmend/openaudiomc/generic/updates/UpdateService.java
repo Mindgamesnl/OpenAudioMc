4b0fdbf653c011740803fdc68f49570f8a8f97b2
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-04 23:36:43 +0200

    basic update service

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/updates/UpdateService.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/updates/UpdateService.java
@@ -1,10 +1,26 @@
 package com.craftmend.openaudiomc.generic.updates;
 
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.networking.rest.RestRequest;
+import com.craftmend.openaudiomc.generic.networking.rest.endpoints.RestEndpoint;
 import com.craftmend.openaudiomc.generic.updates.models.ProjectStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 public class UpdateService {
 
     // project status
-    private ProjectStatus projectStatus = null;
+    @Getter private ProjectStatus projectStatus = null;
+
+    private void updateProjectStatus() {
+        OpenAudioMc.getInstance().getTaskProvider().runAsync(() -> {
+            projectStatus = new RestRequest(RestEndpoint.GITHUB_VERSION_CHECK)
+                    .executeSync()
+                    .getResponse(ProjectStatus.class);
+        });
+    }
+
+
 
 }
