8fe06800cdd5fbbf310345fa402acb772c0bc154
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-04 23:29:09 +0200

    Refactor and add endpoints

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/rest/endpoints/RestEndpoint.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/rest/endpoints/RestEndpoint.java
@@ -4,17 +4,20 @@ import com.craftmend.openaudiomc.OpenAudioMc;
 
 public enum RestEndpoint {
 
-    ENDPOINT_REGISTER("/api/v1/servers/register", false),
-    ENDPOINT_LOGIN("/api/v1/servers/login/_private_key_", true),
-    ENDPOINT_LOGOUT("/api/v1/servers/logout/_private_key_", true),
+    // EXTERNAL SOURCES
+    GITHUB_VERSION_CHECK("https://raw.githubusercontent.com/Mindgamesnl/OpenAudioMc/master/plugin/protocol/static-resources/project_status.json", false),
 
-    ENDPOINT_PLUS_GENTOKEN("/api/v1/servers/createlogin", false), // POST
-    ENDPOINT_PLUS_GET_SETTINGS("/api/v1/public/settings/_public_key_", true),
-    ENDPOINT_PLUS_SAVE_SETTINGS("/api/v1/plus/settings", false), // POST
-    ENDPOINT_PLUS_UPDATE_PLAYERS("/api/v1/plus/players", false); // ALSO POST
+    // OPENAUDIOMC REST API'S
+    ENDPOINT_REGISTER("http://plus.openaudiomc.net/api/v1/servers/register", false),
+    ENDPOINT_LOGIN("http://plus.openaudiomc.net/api/v1/servers/login/_private_key_", true),
+    ENDPOINT_LOGOUT("http://plus.openaudiomc.net/api/v1/servers/logout/_private_key_", true),
+    ENDPOINT_PLUS_GENERATE_TOKEN("http://plus.openaudiomc.net/api/v1/servers/createlogin", false), // POST
+    ENDPOINT_PLUS_GET_SETTINGS("http://plus.openaudiomc.net/api/v1/public/settings/_public_key_", true),
+    ENDPOINT_PLUS_SAVE_SETTINGS("http://plus.openaudiomc.net/api/v1/plus/settings", false), // POST
+    ENDPOINT_PLUS_UPDATE_PLAYERS("http://plus.openaudiomc.net/api/v1/plus/players", false); // ALSO POST
 
     private String url;
-    private boolean translate;
+    private final boolean translate;
     RestEndpoint(String url, boolean translate) {
         this.url = url;
         this.translate = translate;
