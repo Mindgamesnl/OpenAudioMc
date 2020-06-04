1c1e451b52e5079bf413c34e2f0fc20834ae30de
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-04 23:42:14 +0200

    basic checks and setup

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/updates/models/ProjectStatus.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/updates/models/ProjectStatus.java
@@ -1,12 +1,16 @@
 package com.craftmend.openaudiomc.generic.updates.models;
 
+import com.craftmend.openaudiomc.OpenAudioMc;
 import com.google.gson.annotations.SerializedName;
 
 public class ProjectStatus {
 
     @SerializedName("versioning")
     private VersionDetails update;
-
     private Annoucement annoucement;
+
+    public boolean isLocalLatest() {
+        return OpenAudioMc.getInstance().getInvoker().getPluginVersion().equals(update.getVersionTag());
+    }
 
 }
