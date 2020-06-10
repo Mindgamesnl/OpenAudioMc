36c118a7710845ec71e057dded5fed124da69ff5
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-11 00:04:49 +0200

    Implement slave version

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/interfaces/VoiceManagerImplementation.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/interfaces/VoiceManagerImplementation.java
@@ -1,4 +1,15 @@
 package com.craftmend.openaudiomc.generic.voicechat.interfaces;
 
-public class VoiceManagerImplementation {
+import com.craftmend.openaudiomc.generic.voicechat.api.models.MinecraftAccount;
+import com.craftmend.openaudiomc.generic.voicechat.api.util.Task;
+import com.craftmend.openaudiomc.generic.voicechat.room.objects.VoiceRoom;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface VoiceManagerImplementation {
+
+    Task<VoiceRoom> requestVoiceRoomAsync(List<MinecraftAccount> members);
+    VoiceRoom findRoomByPlayer(UUID playerUuid);
+
 }
