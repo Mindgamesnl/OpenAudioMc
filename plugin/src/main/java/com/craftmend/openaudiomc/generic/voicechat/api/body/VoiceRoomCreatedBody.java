a81e927040ac9ac91e5ae5668f01b3150e128570
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-10 22:14:58 +0200

    concurrency

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/api/body/VoiceRoomCreatedBody.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/api/body/VoiceRoomCreatedBody.java
@@ -1,4 +1,30 @@
 package com.craftmend.openaudiomc.generic.voicechat.api.body;
 
-public class VoiceRoomCreatedBody {
+import com.craftmend.openaudiomc.generic.networking.rest.interfaces.AbstractRestResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Basically
+ *
+ * type RoomCreatedBody struct {
+ * 	VoiceServerId string `json:"voiceServerId"`
+ * 	CreatedRoomId string `json:"roomId"`
+ * 	CreatedRoomPassword string `json:"roomApiKey"`
+ * 	VoiceSocket string `json:"voiceSocket"`
+ * }
+ *
+ * from the backend
+ */
+@Getter
+@NoArgsConstructor
+public class VoiceRoomCreatedBody extends AbstractRestResponse {
+
+    private UUID voiceServerId;
+    private String roomId;
+    private String roomApiKey;
+    private String voiceSocket; // endpoint
+
 }
