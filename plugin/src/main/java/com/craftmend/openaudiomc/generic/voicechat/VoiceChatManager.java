e715b875e71281fd08fdbe1cd34a7f9654ff5330
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-10 15:49:47 +0200

    basic room setup

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/VoiceChatManager.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/VoiceChatManager.java
@@ -1,7 +1,13 @@
 package com.craftmend.openaudiomc.generic.voicechat;
 
+import com.craftmend.openaudiomc.generic.voicechat.room.objects.VoiceRoom;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class VoiceChatManager {
 
-
+    private Map<UUID, VoiceRoom> rooms = new HashMap<>();
 
 }
--- plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/room/objects/VoiceRoom.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/voicechat/room/objects/VoiceRoom.java
@@ -1,18 +1,23 @@
 package com.craftmend.openaudiomc.generic.voicechat.room.objects;
 
 import com.craftmend.openaudiomc.generic.voicechat.api.drivers.VoiceRoomDriver;
+import com.craftmend.openaudiomc.generic.voicechat.api.models.MinecraftAccount;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.UUID;
 
 public class VoiceRoom {
 
+    private List<MinecraftAccount> members;
     @Getter private UUID roomId;
     @Getter private VoiceRoomDriver driver;
 
-    public VoiceRoom(UUID roomId, VoiceRoomDriver driver) {
+    public VoiceRoom(UUID roomId, VoiceRoomDriver driver, List<MinecraftAccount> members) {
+        if (members.isEmpty()) throw new IllegalStateException("A room must have some members, might as well do nothing.");
         this.roomId = roomId;
         this.driver = driver;
+        this.members = members;
     }
 
 }
