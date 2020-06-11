cb4f3f98f13013cc5ad19bf932a5c5bdbb4b972c
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-11 11:11:31 +0200

    fix AbstractRestResponse type

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/plus/response/PlusLoginToken.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/plus/response/PlusLoginToken.java
@@ -1,9 +1,10 @@
 package com.craftmend.openaudiomc.generic.plus.response;
 
+import com.craftmend.openaudiomc.generic.networking.rest.interfaces.AbstractRestResponse;
 import lombok.Getter;
 
 @Getter
-public class PlusLoginToken {
+public class PlusLoginToken extends AbstractRestResponse {
 
     private String token;
 
