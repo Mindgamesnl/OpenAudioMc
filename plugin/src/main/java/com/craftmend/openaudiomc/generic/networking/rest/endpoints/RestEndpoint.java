e9862c2aca7c3561a05dca472d27dd4eb5d2297b
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-06-29 11:34:27 +0200

    Endpoints can be HTTPS

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/rest/endpoints/RestEndpoint.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/rest/endpoints/RestEndpoint.java
@@ -9,20 +9,21 @@ public enum RestEndpoint {
     GITHUB_VERSION_CHECK("https://raw.githubusercontent.com/Mindgamesnl/OpenAudioMc/master/plugin/protocol/static-resources/project_status.json", false),
 
     // OPENAUDIOMC REST API'S
-    ENDPOINT_REGISTER("http://plus.openaudiomc.net/api/v1/servers/register", false),                     // GET
-    ENDPOINT_LOGIN("http://plus.openaudiomc.net/api/v1/servers/login/_private_key_", true),              // GET
-    ENDPOINT_LOGOUT("http://plus.openaudiomc.net/api/v1/servers/logout/_private_key_", true),            // GET
-    ENDPOINT_PLUS_GENERATE_TOKEN("http://plus.openaudiomc.net/api/v1/servers/createlogin", false),       // POST
-    ENDPOINT_PLUS_GET_SETTINGS("http://plus.openaudiomc.net/api/v1/public/settings/_public_key_", true), // GET
-    ENDPOINT_PLUS_SAVE_SETTINGS("http://plus.openaudiomc.net/api/v1/plus/settings", false),              // POST
-    ENDPOINT_PLUS_UPDATE_PLAYERS("http://plus.openaudiomc.net/api/v1/plus/players", false),              // ALSO POST
+    ENDPOINT_REGISTER("https://plus.openaudiomc.net/api/v1/servers/register", false),                     // GET
+    ENDPOINT_LOGIN("https://plus.openaudiomc.net/api/v1/servers/login/_private_key_", true),              // GET
+    ENDPOINT_LOGOUT("https://plus.openaudiomc.net/api/v1/servers/logout/_private_key_", true),            // GET
+    ENDPOINT_PLUS_GENERATE_TOKEN("https://plus.openaudiomc.net/api/v1/servers/createlogin", false),       // POST
+    ENDPOINT_PLUS_GET_SETTINGS("https://plus.openaudiomc.net/api/v1/public/settings/_public_key_", true), // GET
+    ENDPOINT_PLUS_SAVE_SETTINGS("https://plus.openaudiomc.net/api/v1/plus/settings", false),              // POST
+    ENDPOINT_PLUS_UPDATE_PLAYERS("https://plus.openaudiomc.net/api/v1/plus/players", false),              // ALSO POST
 
     // VOICE SHIT
-    ENDPOINT_VOICE_CREATE("/api/v1/servers/create-voice/_private_key_", true),                   // GET
+    ENDPOINT_VOICE_CREATE("https://plus.openaudiomc.net/api/v1/servers/create-voice/_private_key_", true),                   // GET
     ;
 
     private String url;
     private final boolean translate;
+
     RestEndpoint(String url, boolean translate) {
         this.url = url;
         this.translate = translate;
