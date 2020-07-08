dad156db931120c41abce7cef42a2a9d247c52d7
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-07-08 16:43:29 +0200

    Publish url when the setup completes

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/commands/subcommands/AcceptSubCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/commands/subcommands/AcceptSubCommand.java
@@ -4,6 +4,7 @@ import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.generic.commands.interfaces.GenericExecutor;
 import com.craftmend.openaudiomc.generic.commands.interfaces.SubCommand;
 import com.craftmend.openaudiomc.generic.core.storage.enums.StorageKey;
+import com.craftmend.openaudiomc.generic.networking.client.objects.player.ClientConnection;
 
 public class AcceptSubCommand extends SubCommand {
 
@@ -17,5 +18,11 @@ public class AcceptSubCommand extends SubCommand {
         OpenAudioMc.getInstance().getConfigurationImplementation().setBoolean(StorageKey.LEGAL_ACCEPTED_TOS_AND_PRIVACY, true);
         OpenAudioMc.getInstance().getConfigurationImplementation().saveAll();
         sender.sendMessage(OpenAudioMc.getInstance().getCommandModule().getCommandPrefix() + "Welcome to OpenAudioMc! you accepted the terms, enjoy the service!");
+
+        OpenAudioMc.getInstance().getNetworkingService().connectIfDown();
+
+        for (ClientConnection client : OpenAudioMc.getInstance().getNetworkingService().getClients()) {
+            client.publishUrl();
+        }
     }
 }
