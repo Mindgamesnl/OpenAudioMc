06519c516296e4a7ccfe5efd13ee2317bdc2c2e5
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-07-09 17:35:55 +0200

    add auth headers and process

--- client/src/js/modules/media/objects/Sound.js
+++ client/src/js/modules/media/objects/Sound.js
@@ -1,33 +1,11 @@
 import * as PluginChannel from "../../../helpers/protocol/PluginChannel";
+import {AudioSourceProcessor} from "../../../helpers/protocol/AudioSourceProcessor";
 
-export class Sound {
+export class Sound extends AudioSourceProcessor{
 
     constructor(source) {
-        // work around for the old google docs system, for those who didn't update yet
-        if (source.includes("http://docs.google.com/uc?export=open&id=")) {
-            let craftmendProxy = source.replace("http://docs.google.com/uc?export=open&id=", "https://openaudio-google-proxy.craftmend.workers.dev/?id=")
-            source = craftmendProxy;
-        }
-        if (source.includes("https://docs.google.com/uc?export=open&id=")) {
-            let craftmendProxy = source.replace("https://docs.google.com/uc?export=open&id=", "https://openaudio-google-proxy.craftmend.workers.dev/?id=")
-            source = craftmendProxy;
-        }
-
-        // handle youtube proxy, if peeps are interested in that but don't know how to
-        // basically for those who can't or wont read documentatino lmao
-        this.isYoutube = false;
-        if (source.includes("youtube") || source.includes("youtu.be")) {
-            let ytId = source.split("v=")[1]
-            source = "https://fetch-yt.craftmend.workers.dev/?v=" + ytId;
-            this.isYoutube = true;
-        }
-
-        // if the page is SSL, but source is http, then proxy it, but only if it is http at all
-        if (location.protocol === 'https:') {
-            if (source.includes("http") && !source.includes("https://")) {
-                source = "https://dark-mouse-53ea.craftmend.workers.dev/corsproxy/?apiurl=" + source;
-            }
-        }
+        super()
+        source = this.translate(source);
 
         this.soundElement = new Audio();
 
