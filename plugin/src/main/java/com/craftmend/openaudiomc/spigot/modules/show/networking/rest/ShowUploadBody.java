0141bc5504c728943db8d0b81ac8f3279e8366f6
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-07-12 21:14:45 +0200

    fix lol

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/networking/rest/ShowUploadBody.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/networking/rest/ShowUploadBody.java
@@ -15,7 +15,6 @@ public class ShowUploadBody {
 
     private String playerUuid;         // player uuid
     private String playerName;         // player name
-    private String name;               // show name
     private Show show;           // serialized show
     private PlusSocketSession session; // for socket authentication
 
