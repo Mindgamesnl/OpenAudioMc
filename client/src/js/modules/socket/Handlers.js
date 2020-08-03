e90ba2bb5690afe9826a54906ccb72662e2b13d7
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-03 12:05:19 +0200

    Prompt update

--- client/src/js/modules/socket/Handlers.js
+++ client/src/js/modules/socket/Handlers.js
@@ -114,6 +114,20 @@ export class Handlers {
                 console.log("[OpenAudioMc] PROTO rev => 3, enabling youtube callbacks");
                 openAudioMc.socketModule.supportsYoutube = true;
             }
+
+            // outdated? lets check it
+            if (revision < 3) {
+                this.requestBox = new AlertBox('#alert-area', {
+                    closeTime: 60000,
+                    persistent: false,
+                    hideCloseButton: true,
+                });
+
+                this.requestBox.show(
+                    '<div style="text-align: center;"><b>Warning!</b> Your installation of OpenAudioMc seems to be pretty outdated. Please download the most recent version from Spigot and install it in your server.' +
+                    '<br/><br/><a href="https://www.spigotmc.org/resources/openaudiomc-music-speakers-regions-bungeecord.30691/" class="alert-message-button">Download page</a></div>'
+                );
+            }
         });
 
         registerHandler("ClientVolumePayload", data => {
