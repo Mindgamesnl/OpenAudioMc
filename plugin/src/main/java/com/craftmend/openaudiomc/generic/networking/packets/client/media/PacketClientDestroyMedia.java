a8afc67a5d131ba21a293d606c59bec2207e95b4
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-04 00:12:12 +0200

    Default to 500MS because that's just a lot nicer tbh

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/packets/client/media/PacketClientDestroyMedia.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/packets/client/media/PacketClientDestroyMedia.java
@@ -6,7 +6,7 @@ import com.craftmend.openaudiomc.generic.networking.payloads.client.media.Client
 
 public class PacketClientDestroyMedia extends AbstractPacket {
 
-    public static final int DEFAULT_FADE_TIME = 250;
+    public static final int DEFAULT_FADE_TIME = 500;
 
     public PacketClientDestroyMedia(String soundId, boolean deleteSpecial, int fadeTime) {
         super(new ClientDestroyMediaPayload(soundId, deleteSpecial, fadeTime), PacketChannel.CLIENT_OUT_DESTROY_MEDIA, null);
