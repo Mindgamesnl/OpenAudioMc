22849839f8bb0a86d4c21066f2d0c542b64967e2
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-05 13:23:42 +0200

    Api refactoring and docs

--- plugin/src/main/java/com/craftmend/openaudiomc/api/interfaces/AudioApi.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/api/interfaces/AudioApi.java
@@ -3,14 +3,51 @@ package com.craftmend.openaudiomc.api.interfaces;
 import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.api.impl.DefaultApi;
 import lombok.Getter;
+import org.jetbrains.annotations.Nullable;
 
-public class AudioApi {
+import java.util.Collection;
+import java.util.UUID;
 
-    @Getter private static OpenAudioMcApi instance;
+public interface AudioApi {
 
-    public static void boot(OpenAudioMc booted) {
-        if (instance != null) throw new IllegalStateException("AudioAPI already initialized");
-        instance = new DefaultApi(booted);
+    /**
+     * Fetch a client instance for a player by UUID.
+     * Only available two ticks after joining.
+     *
+     * @param uuid Player UUID
+     * @return Client instance
+     */
+    @Nullable
+    Client getClient(UUID uuid);
+
+    /**
+     * Fetch all clients that are currently known to the server
+     * @return All clients
+     */
+    Collection<Client> getAllClients();
+
+
+    /**
+     * Get the World Api (used for regions and speakers)
+     * @return World API instance
+     */
+    WorldApi getWorldApi();
+
+    /**
+     * Get the media API instance (used for media management)
+     * @return Media API instance
+     */
+    MediaApi getMediaApi();
+
+
+    /**
+     * Get the registry API instance, used to hook into openaudio
+     * on a low level
+     * @return Registry api instance
+     */
+    RegistryApi getRegistryApi();
+
+    public static AudioApi getInstance() {
+        return new DefaultApi(OpenAudioMc.getInstance());
     }
-
 }
