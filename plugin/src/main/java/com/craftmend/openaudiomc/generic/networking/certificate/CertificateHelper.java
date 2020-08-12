8eb005d871364f580467bc493eb3fb6ef42fdc15
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-08-12 22:38:49 +0200

    Should always trust it

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/certificate/CertificateHelper.java
@@ -0,0 +1,54 @@
+package com.craftmend.openaudiomc.generic.networking.certificate;
+
+import com.craftmend.openaudiomc.generic.core.logging.OpenAudioLogger;
+import okhttp3.OkHttpClient;
+
+import javax.net.ssl.*;
+import java.security.cert.CertificateException;
+
+public class CertificateHelper {
+
+    public static OkHttpClient.Builder ignore(OkHttpClient.Builder builder) {
+        OpenAudioLogger.toConsole("Attempting to middleman ssl");
+        try {
+
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                                throws CertificateException {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            OpenAudioLogger.toConsole("Failed to middleman ssl, should probably be fine");
+        }
+        return builder;
+    }
+
+}
