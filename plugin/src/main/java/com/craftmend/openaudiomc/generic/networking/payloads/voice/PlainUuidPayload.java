7b7ec736d7f57749d2cc1eb221845704b508169c
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-16 22:55:29 +0200

    Minor birthday progress

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/payloads/voice/PlainUuidPayload.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/payloads/voice/PlainUuidPayload.java
@@ -1,6 +1,7 @@
-package com.craftmend.openaudiomc.generic.networking.payloads;
+package com.craftmend.openaudiomc.generic.networking.payloads.voice;
 
 import com.craftmend.openaudiomc.generic.networking.abstracts.AbstractPacketPayload;
+import com.craftmend.openaudiomc.generic.networking.rest.interfaces.AbstractRestResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,8 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AcknowledgeClientPayload extends AbstractPacketPayload {
+public class PlainUuidPayload extends AbstractRestResponse {
 
     private UUID uuid;
-    private String token;
 
 }
