46fb9285733f597e0b9af71e425f6f085d3dd946
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-08-26 11:01:10 +0200

    Entity executors for shows

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/interfaces/ShowRunnable.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/interfaces/ShowRunnable.java
@@ -2,14 +2,47 @@ package com.craftmend.openaudiomc.spigot.modules.show.interfaces;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public abstract class ShowRunnable implements Runnable {
+
+    private final static transient Map<String, Entity> executorEntityCache = new HashMap<>();
 
     public ShowRunnable() {}
 
     abstract public void prepare(String serialize, World world);
     abstract public String serialize();
     @Setter @Getter private boolean executedFromRedis = false;
+
+    protected Entity getExecutorEntity(String world) {
+        Entity fromCache = executorEntityCache.get(world);
+
+        if (fromCache != null && !fromCache.isDead() && fromCache.getLocation().getChunk().isLoaded()) {
+            return fromCache;
+        }
+
+        for (Chunk loadedChunk : Objects.requireNonNull(Bukkit.getWorld(world)).getLoadedChunks()) {
+            for (Entity entity : loadedChunk.getEntities()) {
+                // filter players out
+                if (!(entity instanceof Player)) {
+                    // must be alive
+                    if (!entity.isDead()) {
+                        executorEntityCache.put(world, entity);
+                        return entity;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
 
 }
--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/runnables/CommandRunnable.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/show/runnables/CommandRunnable.java
@@ -2,12 +2,12 @@ package com.craftmend.openaudiomc.spigot.modules.show.runnables;
 
 import com.craftmend.openaudiomc.generic.redis.packets.ExecuteCommandPacket;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
-import com.craftmend.openaudiomc.spigot.modules.show.fake.MockExecutor;
 import com.craftmend.openaudiomc.spigot.modules.show.interfaces.ShowRunnable;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,13 +15,11 @@ public class CommandRunnable extends ShowRunnable {
 
     private String command;
     private String worldName;
-    private transient MockExecutor mockExecutor;
 
     @Override
     public void prepare(String serialized, World world) {
         this.command = serialized;
         this.worldName = world.getName();
-        this.mockExecutor = new MockExecutor(worldName);
         if (this.command.startsWith("/")) this.command = this.command.replace("/" , "");
     }
 
@@ -34,21 +32,18 @@ public class CommandRunnable extends ShowRunnable {
     public void run() {
         if (!isExecutedFromRedis() && !command.toLowerCase().startsWith("oa show")) new ExecuteCommandPacket(command).send();
 
-        Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> {
-            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
-        });
+        Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command));
 
-        /*
-        todo, figure out why console senders autistic
         if (worldName == null) {
-            Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> {
-                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command);
-            });
+            Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command));
         } else {
-            Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> {
-                Bukkit.getServer().dispatchCommand(mockExecutor, command);
-            });
+            Entity executor = getExecutorEntity(worldName);
+
+            if (executor == null) {
+                throw new IllegalStateException("There is no entity loaded to execute the show trigger");
+            }
+
+            Bukkit.getScheduler().runTask(OpenAudioMcSpigot.getInstance(), () -> Bukkit.getServer().dispatchCommand(executor, command));
         }
-        */
     }
 }
