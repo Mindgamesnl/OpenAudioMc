e523d2508cd6b4d160deead7627b8e305e1ff008
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-12-09 23:39:14 +0100

    Implement predicate checks

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/enums/ExtraSpeakerOptions.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/enums/ExtraSpeakerOptions.java
@@ -1,14 +1,42 @@
 package com.craftmend.openaudiomc.spigot.modules.speakers.enums;
 
 import com.craftmend.openaudiomc.spigot.modules.speakers.objects.Speaker;
+import lombok.Getter;
 
-public enum  ExtraSpeakerOptions {
+import java.util.Arrays;
+import java.util.function.Predicate;
 
-    PROCESS_OBSTRUCTIONS;
+public enum ExtraSpeakerOptions {
 
-    public boolean matches(Speaker speaker) {
-        // no extra logic other than checking if the option is enabled
-        // TODO: add logic that only 3d speakers can have obstruction processing
+    IGNORE_SYNCHRONIZATION(
+            "Ignore Synchronization",
+            "Disables audio syncronization for this speaker specifically",
+            speaker -> true
+    ),
+
+
+    PROCESS_OBSTRUCTIONS(
+            "Process Obstructions",
+            "Muffles sound based on how many walls / obstructions it has (only for 3D speakers)",
+            speaker -> speaker.getSpeakerType() == SpeakerType.SPEAKER_3D
+    ),
+    ;
+
+    @Getter private String title;
+    @Getter private String description;
+    private Predicate<Speaker>[] predicates;
+
+    ExtraSpeakerOptions(String title, String description, Predicate<Speaker>... requirementChecks) {
+        this.title = title;
+        this.description = description;
+        this.predicates = requirementChecks;
+    }
+
+    public boolean isAllowedFor(Speaker speaker) {
+        return Arrays.stream(this.predicates).allMatch(predicate -> predicate.test(speaker));
+    }
+
+    public boolean isEnabledFor(Speaker speaker) {
         return speaker.getExtraOptions().contains(this);
     }
 
