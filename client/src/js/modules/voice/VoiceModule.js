3d91be8ae1da8ad944e2cd438ab08f9c0bc9ddae
Mindgamesnl <matsmoolhuizen@gmail.com>
2021-01-15 00:06:17 +0100

    Incomming voice streams

--- client/src/js/modules/voice/VoiceModule.js
+++ client/src/js/modules/voice/VoiceModule.js
@@ -24,7 +24,7 @@ export class VoiceModule {
     }
 
     addPeer(playerUuid, playerName, playerStreamKey) {
-        this.peerMap.set(playerStreamKey, new VoicePeer(this.openAudioMc, playerName, playerUuid, playerStreamKey));
+        this.peerMap.set(playerStreamKey, new VoicePeer(this.openAudioMc, playerName, playerUuid, playerStreamKey, this.server));
     }
 
     removePeer(key) {
--- client/src/js/modules/voice/peer/VoicePeer.js
+++ client/src/js/modules/voice/peer/VoicePeer.js
@@ -1,12 +1,15 @@
 import {VoicePeerUi} from "./VoicePeerUi";
+import {IncomingVoiceStream} from "./IncomingVoiceStream";
 
 export class VoicePeer {
 
-    constructor(openAudioMc, playerName, playerUuid, streamKey) {
+    constructor(openAudioMc, playerName, playerUuid, streamKey, server) {
         this.openAudioMc = openAudioMc;
         this.playerName = playerName;
         this.playerUuid = playerName;
         this.streamKey = streamKey;
+        this.active = true;
+        this.ready = false;
 
         this.volume = 80;
 
@@ -22,10 +25,26 @@ export class VoicePeer {
             Cookies.set("vc-volume-of-" + playerName, newVolume, { expires: 30 });
 
             // todo: update actual volume from the stream
+            if (this.ready) {
+                this.stream.setVolume(this.volume);
+            }
+        })
+
+        this.stream = new IncomingVoiceStream(openAudioMc, server, openAudioMc.voiceModule.streamKey, streamKey);
+        this.stream.start(() => {
+            // am I actually too late?
+            if (!this.active) {
+                this.stream.stop();
+                return
+            }
+
+            this.stream.setVolume(this.volume);
+            this.ready = true;
         })
     }
 
     stop() {
+        this.active = false;
         this.ui.remove()
     }
 
