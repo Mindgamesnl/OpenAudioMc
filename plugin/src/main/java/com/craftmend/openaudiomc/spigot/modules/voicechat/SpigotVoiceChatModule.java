a11de9ad0195d6825e30d03cb18fc8a57d52daaa
Mindgamesnl <mats@craftmend.com>
2021-03-27 10:56:49 +0100

    Refactor messages to use the new system

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/SpigotVoiceChatModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/SpigotVoiceChatModule.java
@@ -2,8 +2,12 @@ package com.craftmend.openaudiomc.spigot.modules.voicechat;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.api.impl.event.events.AccountAddTagEvent;
+import com.craftmend.openaudiomc.api.impl.event.events.PlayerEnterVoiceProximityEvent;
+import com.craftmend.openaudiomc.api.impl.event.events.PlayerLeaveVoiceProximityEvent;
+import com.craftmend.openaudiomc.api.impl.event.events.enums.VoiceEventCause;
 import com.craftmend.openaudiomc.api.interfaces.AudioApi;
 import com.craftmend.openaudiomc.generic.craftmend.enums.CraftmendTag;
+import com.craftmend.openaudiomc.generic.platform.Platform;
 import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
 import com.craftmend.openaudiomc.spigot.modules.voicechat.filters.PeerFilter;
@@ -19,6 +23,7 @@ public class SpigotVoiceChatModule {
 
     public SpigotVoiceChatModule(OpenAudioMcSpigot openAudioMcSpigot) {
 
+        // enable voice chat when the tag gets added
         AudioApi.getInstance().getEventDriver()
                 .on(AccountAddTagEvent.class)
                 .setHandler(handler -> {
@@ -32,5 +37,39 @@ public class SpigotVoiceChatModule {
                     }
                     firstRun = false;
                 });
+
+        // register events to notify players when player enter, leave, and whatever
+        AudioApi.getInstance().getEventDriver()
+                .on(PlayerEnterVoiceProximityEvent.class)
+                .setHandler(event -> {
+                    // skip if this is disabled in the settings
+                    if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
+
+                    // only notify normal events, we don't really care about special things
+                    if (event.getCause() != VoiceEventCause.NORMAL) return;
+
+                    // a player entered the radius and is now listening
+                    event.getSpeaker().getPlayer().sendMessage(Platform.translateColors(
+                            StorageKey.MESSAGE_VC_USER_ADDED.getString()
+                                    .replace("%name", event.getListener().getOwnerName())
+                    ));
+                });
+
+        AudioApi.getInstance().getEventDriver()
+                .on(PlayerLeaveVoiceProximityEvent.class)
+                .setHandler(event -> {
+                    // skip if this is disabled in the settings
+                    if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
+
+                    // only notify normal events, we don't really care about special things
+                    if (event.getCause() != VoiceEventCause.NORMAL) return;
+
+                    // a player left proximity, goodbye!
+                    event.getSpeaker().getPlayer().sendMessage(Platform.translateColors(
+                            StorageKey.MESSAGE_VC_USER_LEFT.getString()
+                                    .replace("%name", event.getListener().getOwnerName())
+                    ));
+                });
+
     }
 }
