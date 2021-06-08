713688d92c6f4ca16a1c4ab5619387e7ddc1b09d
Mindgamesnl <mats@craftmend.com>
2021-06-08 09:19:25 +0200

    Add a voicechat debug state

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/state/collectors/VoiceDetail.java
@@ -0,0 +1,40 @@
+package com.craftmend.openaudiomc.generic.state.collectors;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.state.interfaces.StateDetail;
+import com.craftmend.openaudiomc.generic.voicechat.DefaultVoiceServiceImpl;
+import com.craftmend.openaudiomc.generic.voicechat.VoiceService;
+
+public class VoiceDetail implements StateDetail {
+    @Override
+    public String title() {
+        return "VoiceRTP";
+    }
+
+    @Override
+    public String value() {
+        // is it even loaded
+        if (OpenAudioMc.getInstance().getCraftmendService().getVoiceService() == null) {
+            return "No service";
+        }
+
+        VoiceService voiceService = OpenAudioMc.getInstance().getCraftmendService().getVoiceService();
+
+        if (voiceService.isEnabled()) {
+            if (voiceService.getDriver() == null) {
+                return "No driver";
+            } else {
+                if (voiceService instanceof DefaultVoiceServiceImpl) {
+                    DefaultVoiceServiceImpl i = (DefaultVoiceServiceImpl) voiceService;
+                    return "Using " + i.getUsedSlots() + " out of " + i.getAllowedSlots() + " slots.";
+                } else {
+                    return "Unknown service type";
+                }
+            }
+        } else {
+            return "Disabled";
+        }
+
+        return null;
+    }
+}
