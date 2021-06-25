4ccb410df0c6af4cb99469f443d248cf59f02e98
Mindgamesnl <mats@craftmend.com>
2021-06-25 14:30:31 +0200

    Typos lol

--- plugin/src/main/java/com/craftmend/openaudiomc/api/impl/event/enums/EventSupport.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/api/impl/event/enums/EventSupport.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 public enum EventSupport {
 
     UNKNOWN("Failed to fetch event support for this event"),
-    SPIGOT_ONLY("This event is anly availible on spigot API instances"),
-    PROXY_ONLY("This feature is only availible through the proxy API instance"),
+    SPIGOT_ONLY("This event is only available on spigot API instances"),
+    PROXY_ONLY("This feature is only available through the proxy API instance"),
     ONLY_PROXY_IF_AVAILABLE("This feature is blocked in the spigot API because the proxy has taken over functionality, please use the proxy API."),
     EVERYWHERE("This error should never appear, since this event is supported everywhere");
 
