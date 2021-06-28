632736b0300d4477fbea183f8fb11d02fae97813
Mindgamesnl <mats@craftmend.com>
2021-06-29 01:28:53 +0200

    Updatd readme

--- plugin/src/test/java/com/craftmend/tests/TestRest.java
+++ plugin/src/test/java/com/craftmend/tests/TestRest.java
@@ -4,14 +4,8 @@ import com.craftmend.openaudiomc.generic.enviroment.models.ProjectStatus;
 import com.craftmend.openaudiomc.generic.networking.rest.RestRequest;
 import com.craftmend.openaudiomc.generic.networking.rest.endpoints.RestEndpoint;
 import com.craftmend.openaudiomc.generic.networking.rest.interfaces.ApiResponse;
-import com.craftmend.openaudiomc.generic.utils.data.GsonFactory;
-import com.craftmend.utils.ClassScanner;
-import com.google.gson.Gson;
-import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.lang.reflect.Field;
 
 public class TestRest {
 
@@ -26,6 +20,5 @@ public class TestRest {
         Assert.assertNotNull(status);
         Assert.assertTrue(status.isLocalLatest());
     }
-
 
 }
