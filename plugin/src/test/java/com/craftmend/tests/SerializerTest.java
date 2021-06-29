82c89f8f6138626d13cb4d14f3bcc1ec9570c728
Mindgamesnl <matsmoolhuizen@gmail.com>
2021-06-29 09:55:01 +0200

    Test all serializable components

--- plugin/src/test/java/com/craftmend/tests/SerializerTest.java
+++ plugin/src/test/java/com/craftmend/tests/SerializerTest.java
@@ -1,5 +1,8 @@
 package com.craftmend.tests;
 
+import com.craftmend.openaudiomc.generic.networking.abstracts.AbstractPacket;
+import com.craftmend.openaudiomc.generic.networking.abstracts.AbstractPacketPayload;
+import com.craftmend.openaudiomc.generic.redis.packets.interfaces.OARedisPacket;
 import com.craftmend.openaudiomc.generic.utils.data.GsonFactory;
 import com.craftmend.utils.ClassScanner;
 import com.google.gson.Gson;
@@ -7,7 +10,8 @@ import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.lang.reflect.Field;
+import java.io.Serializable;
+import java.lang.reflect.Modifier;
 
 public class SerializerTest {
 
@@ -15,20 +19,62 @@ public class SerializerTest {
     @Test
     public void testEvents() {
         Thread.setDefaultUncaughtExceptionHandler(null);
-        Class[] oaClasses = ClassScanner.getClasses("com.craftmend.openaudiomc.generic", new String[]{"Packet", "Payload"});
+        Class[] oaClasses = ClassScanner.getClasses("com.craftmend.openaudiomc.generic", null);
+
+        // test serialization for packets that extend or implement one of these
+        Class[] relatedTo = new Class[]{
+                AbstractPacket.class,
+                AbstractPacketPayload.class,
+                OARedisPacket.class,
+                Serializable.class
+
+        };
 
         Gson gson = GsonFactory.create();
 
         for (Class oaClass : oaClasses) {
-            if (oaClass.getName().contains("enum") || oaClass.getName().contains("abstracts")) continue;
-            System.out.println("Testing serializable component " + oaClass.getName());
+            boolean isSubType = false;
 
-            Object result = gson.fromJson("{}", oaClass);
-            Assert.assertNotNull("The packet can't be null", result);
+            for (Class aClass : relatedTo) {
+                for (Class anInterface : oaClass.getInterfaces()) {
+                    if (anInterface == aClass) {
+                        isSubType = true;
+                    }
+                }
+                Class su = oaClass.getSuperclass();
+                if (su == aClass) {
+                    isSubType = true;
+                }
+            }
 
-            for (Field field : oaClass.getFields()) {
-                field.setAccessible(true);
-                Assert.assertNull("The field should be empty", field.get(result));
+            if (oaClass.getName().contains("Adapter")) {
+                isSubType = false;
+            }
+
+            if (isSubType) {
+                System.out.println("Testing serializable component " + oaClass.getName());
+
+                Object result = gson.fromJson("{}", oaClass);
+                Assert.assertNotNull("The packet can't be null", result);
+
+                if (result instanceof OARedisPacket) {
+                    OARedisPacket packet = (OARedisPacket) result;
+                    String reSerialized = packet.serialize();
+                    Assert.assertNotNull(reSerialized);
+                    Assert.assertEquals("{}", reSerialized);
+                }
+
+                if (result instanceof AbstractPacketPayload) {
+                    // re-serialize
+                    String json = gson.toJson(result);
+                    Assert.assertNotNull(json);
+                }
+
+                if (result instanceof AbstractPacket) {
+                    // re-serialize
+                    String json = gson.toJson(result);
+                    Assert.assertNotNull(json);
+                }
             }
         }
     }
