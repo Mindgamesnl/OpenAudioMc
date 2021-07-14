fd8bf4559a00f2beb7141e9d8c9d28d2224f57fd
Mindgamesnl <matsmoolhuizen@gmail.com>
2021-07-14 21:52:10 +0200

    Implement current messages with the new system

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/SpigotVoiceChatModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/SpigotVoiceChatModule.java
@@ -2,9 +2,12 @@ package com.craftmend.openaudiomc.spigot.modules.voicechat;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.api.impl.event.ApiEventDriver;
+import com.craftmend.openaudiomc.api.impl.event.enums.TickEventType;
 import com.craftmend.openaudiomc.api.impl.event.events.*;
 import com.craftmend.openaudiomc.api.impl.event.enums.VoiceEventCause;
 import com.craftmend.openaudiomc.api.interfaces.AudioApi;
+import com.craftmend.openaudiomc.generic.networking.client.objects.player.ClientConnection;
+import com.craftmend.openaudiomc.generic.networking.client.objects.player.ClientRtcManager;
 import com.craftmend.openaudiomc.generic.platform.Platform;
 import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
@@ -13,11 +16,14 @@ import com.craftmend.openaudiomc.spigot.modules.voicechat.tasks.PlayerProximityT
 import com.craftmend.openaudiomc.spigot.modules.voicechat.tasks.TickVoicePacketQueue;
 import lombok.Getter;
 
+import java.util.UUID;
+
 public class SpigotVoiceChatModule {
 
     @Getter
     private PlayerProximityTicker proximityTicker;
     private boolean firstRun = true;
+    private int broadcastTickLoop = 0;
 
     public SpigotVoiceChatModule(OpenAudioMcSpigot openAudioMcSpigot) {
         ApiEventDriver eventDriver = AudioApi.getInstance().getEventDriver();
@@ -38,32 +44,74 @@ public class SpigotVoiceChatModule {
 
         // register events to notify players when player enter, leave, and whatever
         eventDriver.on(PlayerEnterVoiceProximityEvent.class).setHandler(event -> {
-                    // skip if this is disabled in the settings
-                    if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
+            // skip if this is disabled in the settings
+            if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
 
-                    // only notify normal events, we don't really care about special things
-                    if (event.getCause() != VoiceEventCause.NORMAL) return;
+            // only notify normal events, we don't really care about special things
+            if (event.getCause() != VoiceEventCause.NORMAL) return;
 
-                    // a player entered the radius and is now listening
-                    event.getSpeaker().getPlayer().sendMessage(Platform.translateColors(
-                            StorageKey.MESSAGE_VC_USER_ADDED.getString()
-                                    .replace("%name", event.getListener().getOwnerName())
-                    ));
-                });
+            event.getSpeaker().getClientRtcManager().getRecentPeerAdditions().add(event.getListener().getOwnerUUID());
+            event.getSpeaker().getClientRtcManager().getRecentPeerRemovals().remove(event.getListener().getOwnerUUID());
+        });
 
         eventDriver.on(PlayerLeaveVoiceProximityEvent.class).setHandler(event -> {
-                    // skip if this is disabled in the settings
-                    if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
+            // skip if this is disabled in the settings
+            if (!StorageKey.SETTINGS_VC_ANNOUNCEMENTS.getBoolean()) return;
 
-                    // only notify normal events, we don't really care about special things
-                    if (event.getCause() != VoiceEventCause.NORMAL) return;
+            // only notify normal events, we don't really care about special things
+            if (event.getCause() != VoiceEventCause.NORMAL) return;
 
-                    // a player left proximity, goodbye!
-                    event.getSpeaker().getPlayer().sendMessage(Platform.translateColors(
-                            StorageKey.MESSAGE_VC_USER_LEFT.getString()
-                                    .replace("%name", event.getListener().getOwnerName())
-                    ));
-                });
+            event.getSpeaker().getClientRtcManager().getRecentPeerRemovals().add(event.getListener().getOwnerUUID());
+            event.getSpeaker().getClientRtcManager().getRecentPeerAdditions().remove(event.getListener().getOwnerUUID());
+        });
+
+        // do vc tick loop
+        eventDriver.on(VoiceChatPeerTickEvent.class).setHandler(event -> {
+            if (event.getWhen() == TickEventType.BEFORE_TICK) return;
+            if (broadcastTickLoop > 2) {
+                broadcastTickLoop = 0;
+                return;
+            }
+            broadcastTickLoop++;
+
+            // go over every player and handle their message queue
+            for (ClientConnection client : OpenAudioMc.getInstance().getNetworkingService().getClients()) {
+                ClientRtcManager manager = client.getClientRtcManager();
+                // handle their join messages, if any
+                if (!manager.getRecentPeerAdditions().isEmpty()) {
+                    // do these
+                    if (manager.getRecentPeerRemovals().size() == 1) {
+                        // do single
+                        ClientConnection other = clientFromId(manager.getRecentPeerAdditions().stream().findFirst().get());
+                        if (other != null) {
+                            client.getPlayer().sendMessage(Platform.translateColors(
+                                    StorageKey.MESSAGE_VC_USER_ADDED.getString()
+                                            .replace("%name", other.getOwnerName())
+                            ));
+                        }
+                    } else {
+                        // do multiple
+                    }
+                }
+
+                if (!manager.getRecentPeerRemovals().isEmpty()) {
+                    // do these
+                    if (manager.getRecentPeerRemovals().size() == 1) {
+                        // do single
+                        ClientConnection other = clientFromId(manager.getRecentPeerRemovals().stream().findFirst().get());
+                        if (other != null) {
+                            client.getPlayer().sendMessage(Platform.translateColors(
+                                    StorageKey.MESSAGE_VC_USER_LEFT.getString()
+                                            .replace("%name", other.getOwnerName())
+                            ));
+                        }
+                    } else {
+                        // do multiple
+                    }
+                }
+
+            }
+        });
 
         // mute messages
         eventDriver.on(MicrophoneMuteEvent.class).setHandler(event -> {
@@ -75,6 +123,9 @@ public class SpigotVoiceChatModule {
             if (!event.getClient().isConnected()) return;
             event.getClient().getPlayer().sendMessage(Platform.translateColors(StorageKey.MESSAGE_VC_MIC_UNMUTE.getString()));
         });
+    }
 
+    private ClientConnection clientFromId(UUID id) {
+        return OpenAudioMc.getInstance().getNetworkingService().getClient(id);
     }
 }
