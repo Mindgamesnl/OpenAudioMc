8b37041b013d508b4bac14b2c51ce8a7159d1f56
Mindgamesnl <mats@craftmend.com>
2021-07-16 00:45:36 +0200

    Receive and re-use configs from proxies

--- plugin/src/main/java/com/craftmend/openaudiomc/api/impl/event/events/ConfigurationPushEvent.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/api/impl/event/events/ConfigurationPushEvent.java
@@ -3,13 +3,17 @@ package com.craftmend.openaudiomc.api.impl.event.events;
 import com.craftmend.openaudiomc.api.impl.event.NetworkedAudioEvent;
 import com.craftmend.openaudiomc.api.impl.event.enums.EventSupport;
 import com.craftmend.openaudiomc.api.interfaces.EventSupportFlag;
+import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 @EventSupportFlag(support = EventSupport.EVERYWHERE)
 public class ConfigurationPushEvent extends NetworkedAudioEvent {
 
-    public ConfigurationPushEvent(ProxiedPlayer player) {
+    @Getter private String fileContent;
+
+    public ConfigurationPushEvent(ProxiedPlayer player, String fileContent) {
         super(player);
+        this.fileContent = fileContent;
     }
 
     @Override
---
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/proxy/listeners/ModernPacketListener.java
@@ -0,0 +1,26 @@
+package com.craftmend.openaudiomc.spigot.modules.proxy.listeners;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.api.impl.event.events.ConfigurationPushEvent;
+import com.craftmend.openaudiomc.api.impl.event.events.SystemReloadEvent;
+import com.craftmend.openaudiomc.api.interfaces.AudioApi;
+import com.craftmend.openaudiomc.generic.logging.OpenAudioLogger;
+import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
+import com.craftmend.openaudiomc.spigot.modules.configuration.SpigotConfiguration;
+
+public class ModernPacketListener {
+
+    public ModernPacketListener() {
+        AudioApi.getInstance().getEventDriver().on(ConfigurationPushEvent.class)
+                .setHandler(event -> {
+                    OpenAudioLogger.toConsole("Received a config instance from the proxy, updating local resources and reloading...");
+                    OpenAudioMc.getInstance().getResourceManager().getSavedRoot().setLastConfigContent(event.getFileContent());
+                    OpenAudioMc.getInstance().getResourceManager().getSavedRoot().setUseConfigFile(true);
+
+                    SpigotConfiguration configuration = (SpigotConfiguration) OpenAudioMc.getInstance().getConfiguration();
+                    configuration.loadConfig(OpenAudioMcSpigot.getInstance());
+                    AudioApi.getInstance().getEventDriver().fire(new SystemReloadEvent());
+                });
+    }
+
+}
