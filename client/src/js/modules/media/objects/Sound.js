fafc0d0795b1b1cb1305be5814b1bad87a3e6b88
Mindgamesnl <mats@craftmend.com>
2021-07-29 18:43:07 +0200

    Test yt

--- client/src/js/modules/media/objects/Sound.js
+++ client/src/js/modules/media/objects/Sound.js
@@ -42,7 +42,6 @@ export class Sound extends AudioSourceProcessor {
         source = await this.translate(source);
 
         this.soundElement = await GetAudio(source, true, allowCaching);
-        this.soundElement.crossOrigin = "anonymous";
 
         // error handling
         this.soundElement.onerror = (error) => {
