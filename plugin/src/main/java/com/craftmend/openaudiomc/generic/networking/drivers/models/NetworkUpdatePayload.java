d9465f2536f22f2fb9ee103d2dccd0922fd52766
Mindgamesnl <mats@craftmend.com>
2021-08-20 01:25:32 +0200

    Future-proof safety checks

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/drivers/models/NetworkUpdatePayload.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/drivers/models/NetworkUpdatePayload.java
@@ -1,6 +1,19 @@
 package com.craftmend.openaudiomc.generic.networking.drivers.models;
 
+import lombok.Getter;
+
 import java.io.Serializable;
+import java.util.Set;
 
 public class NetworkUpdatePayload implements Serializable {
+
+    /**
+     * Required CraftmendTag's for this event to be executed.
+     * Note that these are being send as STRING and evaluated as such,
+     * this is to prevent errors on servers running old versions of the plugin
+     * that might not support future tags. Running valueOf checks with those
+     * would throw weird exceptions and possibly cause funky behaviour
+     */
+    @Getter private Set<String> requiredTags;
+
 }
