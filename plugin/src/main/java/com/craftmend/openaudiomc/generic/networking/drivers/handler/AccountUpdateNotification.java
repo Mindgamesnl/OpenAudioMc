2079caeafc9bb1ac1b38adb966ab5cf3666ccd3f
Mindgamesnl <mats@craftmend.com>
2021-08-20 11:32:44 +0200

    Support real time settings

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/drivers/handler/AccountUpdateNotification.java
@@ -0,0 +1,26 @@
+package com.craftmend.openaudiomc.generic.networking.drivers.handler;
+
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.commands.CommandService;
+import com.craftmend.openaudiomc.generic.craftmend.CraftmendService;
+import com.craftmend.openaudiomc.generic.logging.OpenAudioLogger;
+import com.craftmend.openaudiomc.generic.networking.client.objects.player.ClientConnection;
+import com.craftmend.openaudiomc.generic.networking.drivers.interfaces.NotificationHandler;
+import com.craftmend.openaudiomc.generic.networking.drivers.models.BackendNotification;
+import com.craftmend.openaudiomc.generic.networking.interfaces.NetworkingService;
+import com.craftmend.openaudiomc.generic.platform.Platform;
+
+/**
+ * Simple handler for account notifications
+ *
+ * this event fires whenever the owning craftmend account pushed an update
+ * so we'll need to reset/pull our data whenever it fires
+ */
+public class AccountUpdateNotification implements NotificationHandler {
+
+    @Override
+    public void handle(BackendNotification notificationData) {
+        OpenAudioLogger.toConsole("Updating craftmend account due to update push..");
+        OpenAudioMc.getService(CraftmendService.class).syncAccount();
+    }
+}
