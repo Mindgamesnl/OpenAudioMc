354c3badf647beb501e7bc0f6f38c38258a63a09
Mindgamesnl <mats@craftmend.com>
2021-08-27 12:22:42 +0200

    Repeat notifications until there are none to see it

--- plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/drivers/handler/AdminNotification.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/generic/networking/drivers/handler/AdminNotification.java
@@ -7,6 +7,9 @@ import com.craftmend.openaudiomc.generic.networking.drivers.interfaces.Notificat
 import com.craftmend.openaudiomc.generic.networking.drivers.models.BackendNotification;
 import com.craftmend.openaudiomc.generic.networking.interfaces.NetworkingService;
 import com.craftmend.openaudiomc.generic.platform.Platform;
+import com.craftmend.openaudiomc.generic.platform.interfaces.TaskService;
+
+import java.util.UUID;
 
 /**
  * Simple handler for admin notifications
@@ -26,5 +29,24 @@ public class AdminNotification implements NotificationHandler {
                 client.getPlayer().sendMessage(message);
             }
         }
+
+        TaskService ts = OpenAudioMc.resolveDependency(TaskService.class);
+        NetworkingService ns = OpenAudioMc.getService(NetworkingService.class);
+
+        // schedule a temporary listener to handle notifications post join
+        String finalMessage = message;
+        UUID subscriber = ns.subscribeToConnections((cc) -> {
+            if (cc.getPlayer().isAdministrator()) {
+                ts.schduleSyncDelayedTask(() -> {
+                    // send the message
+                    cc.getPlayer().sendMessage(finalMessage);
+                }, 25);
+            }
+        });
+
+        // remove the listener in like a minute
+        ts.schduleSyncDelayedTask(() -> {
+            ns.unsubscribeClientEventHandler(subscriber);
+        }, 60 * 20);
     }
 }
