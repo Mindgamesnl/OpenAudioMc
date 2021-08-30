8db8d278175de00de376aef95f9f67663f590250
Mindgamesnl <matsmoolhuizen@gmail.com>
2021-08-30 15:27:04 +0200

    Fix spam

--- plugin/notepad.md
+++ plugin/notepad.md
@@ -2,3 +2,7 @@ Changelog since release:
  - Fixed webrtc
  - You can now transfer addons
  - Added aliases in the registry api
+ - Fixed voice issues
+ - Added admin notifications
+ - Automatic client checks
+ - 
--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/players/listeners/PlayerConnectionListener.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/players/listeners/PlayerConnectionListener.java
@@ -14,7 +14,16 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class PlayerConnectionListener implements Listener {
+
+    private Map<UUID, Instant> playerMuteTimeout = new HashMap<>();
+    private final Instant BOOT = Instant.now();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
@@ -24,11 +33,19 @@ public class PlayerConnectionListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         OpenAudioMc.getService(PlayerService.class).remove(event.getPlayer());
+        playerMuteTimeout.remove(event.getPlayer().getUniqueId());
     }
 
     @EventHandler
     public void onShort(PlayerSwapHandItemsEvent event) {
         if (event.getPlayer().isSneaking() && StorageKey.SETTINGS_VC_TOGGLE_MIC_SWAP.getBoolean()) {
+
+            if (Math.abs(Duration.between(playerMuteTimeout.getOrDefault(event.getPlayer().getUniqueId(), BOOT), Instant.now()).toMillis() / 1000) < 3) {
+                // disable
+                return;
+            }
+
+            playerMuteTimeout.put(event.getPlayer().getUniqueId(), Instant.now());
             SpigotConnection spigotConnection = OpenAudioMc.getService(PlayerService.class).getClient(event.getPlayer().getUniqueId());
 
             if (!spigotConnection.getClientConnection().isConnectedToRtc()) {
