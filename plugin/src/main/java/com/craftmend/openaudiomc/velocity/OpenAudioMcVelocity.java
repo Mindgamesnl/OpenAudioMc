5599c2e2b2ac3f9a726cc10f4270c2ed04a4b51d
Mindgamesnl <mats@craftmend.com>
2021-11-21 22:25:47 +0100

    Plugin update

--- plugin/src/main/java/com/craftmend/openaudiomc/velocity/OpenAudioMcVelocity.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/velocity/OpenAudioMcVelocity.java
@@ -32,7 +32,7 @@ import java.time.Instant;
 @Plugin(
         id = "openaudiomc",
         name = "OpenAudioMc Bungee Plugin Port for Velocity",
-        version = "6.5.7",
+        version = "6.5.7.2",
         authors = {"Mindgamesnl", "fluse1367"},
         description = "The OpenAudioMc plugin. Brings real sound and lights to your minecraft server with the help of a custom web client. Velocity plugin port by fluse1367.",
         url = "https://openaudiomc.net/"
