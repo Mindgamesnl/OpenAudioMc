b7cfed0521a075a4badf47199f83518c920e9bbb
thomas <thomas.weert@icloud.com>
2022-01-16 13:54:07 +0100

    Implemented PlaceholderAPI values: - connect_client: if connected to web client - connect_vc: if connected to voice chat - count_client: how many are connected to the web client - count_vc: how many are connected to the voice chat - count_vc_max: max slots for voice chat - token: current token or /audio if none
    TODO: config migration

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/placeholderapi/PlaceholderModule.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/placeholderapi/PlaceholderModule.java
@@ -1,6 +1,12 @@
 package com.craftmend.openaudiomc.spigot.modules.placeholderapi;
 
+import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.authentication.AuthenticationService;
+import com.craftmend.openaudiomc.generic.craftmend.CraftmendService;
+import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
+import com.craftmend.openaudiomc.spigot.modules.players.SpigotPlayerService;
+import com.craftmend.openaudiomc.spigot.modules.players.objects.SpigotConnection;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
@@ -30,7 +36,73 @@ public class PlaceholderModule extends PlaceholderExpansion {
 
     @Override
     public String onRequest(OfflinePlayer player, @NotNull String params) {
-        return "TODO Implement";
+        if(params.equals("connect_client")) {
+            boolean b = OpenAudioMc.getService(
+                            SpigotPlayerService.class
+                    ).getClient(player.getUniqueId())
+                    .getClientConnection()
+                    .isConnected();
+
+            if(b)
+                return StorageKey.SETTINGS_PAPI_CLIENT_CONNECTED.getString();
+
+            return StorageKey.SETTINGS_PAPI_CLIENT_DISCONNECTED.getString();
+        }
+
+        if(params.equals("connect_vc")) {
+            boolean b = OpenAudioMc.getService(
+                            SpigotPlayerService.class
+                    ).getClient(player.getUniqueId())
+                    .getClientConnection()
+                    .getRtcSessionManager()
+                    .isReady();
+
+            if(b)
+                return StorageKey.SETTINGS_PAPI_VC_CONNECTED.getString();
+
+            return StorageKey.SETTINGS_PAPI_VC_DISCONNECTED.getString();
+        }
+
+        if(params.equals("count_client")) {
+            int clients = 0;
+            for (SpigotConnection spigotConnection : OpenAudioMc.getService(SpigotPlayerService.class).getClients())
+                if (spigotConnection.getClientConnection().isConnected())
+                    clients++;
+
+            return Integer.toString(clients);
+        }
+
+        if(params.equals("count_vc"))
+            return Integer.toString(
+                    OpenAudioMc.getService(
+                            CraftmendService.class
+                    ).getVoiceApiConnection()
+                            .getUsedSlots()
+            );
+
+        if(params.equals("count_vc_max"))
+            return Integer.toString(
+                    OpenAudioMc.getService(
+                                    CraftmendService.class
+                            ).getVoiceApiConnection()
+                            .getMaxSlots()
+            );
+
+        if(params.equals("token")) {
+            String token = OpenAudioMc.getService(
+                            AuthenticationService.class
+                    ).getDriver()
+                    .getSessionCacheMap()
+                    .get(player.getUniqueId())
+                    .getContext();
+
+            if(token == null || token.isEmpty())
+                return "/audio";
+
+            return token;
+        }
+
+        return "invalid parameter";
     }
 
 }
