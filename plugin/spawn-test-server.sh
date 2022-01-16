33a7c17f1b97eb5140070a83c92d053db380a8b4
BuildTools <mats@craftmend.com>
2022-01-16 11:48:31 +0100

    Make starting less verbose

--- plugin/spawn-test-server.sh
+++ plugin/spawn-test-server.sh
@@ -1,14 +1,17 @@
 mkdir -p test-server/plugins/
-mvn clean install -Dmaven.test.skip=true
+echo "Building new OpenAudioMc jar without unit tests.."
+mvn clean install -Dmaven.test.skip=true -q
+rm test-server/plugins/OpenAudioMc-*
 cp target/OpenAudioMc-* test-server/plugins/
 
 cd test-server
-
+echo "Cleaning server..."
 if [ ! -f ./spigot-1.18.1.jar ]; then
     wget https://download.getbukkit.org/spigot/spigot-1.18.1.jar
     echo "eula=true" > eula.txt
     chmod +x spigot-1.18.1.jar
 fi
 
+echo "Starting server.."
 export OA_ENVIRONMENT=DEVELOPMENT
 java -Xms3G -Xmx3G -jar spigot-1.18.1.jar nogui
