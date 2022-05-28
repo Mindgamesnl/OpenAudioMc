c694dfe76b0a8147499b64787ba8b86cd1be8283
Mats <mats@craftmend.com>
2022-05-28 13:53:59 +0200

    New modules

---
+++ dev-resources/bump-binaries.sh
@@ -0,0 +1,12 @@
+[ ! -d "plugin/" ] && echo "Execute this script from the repo root" && exit 1
+
+# Build everything
+mvn clean install
+
+# copy modules
+cp module-src/vistas-server/target/vistas-server.jar modules/vistas-server.jar
+cp module-src/vistas-client/target/vistas-client.jar modules/vistas-client.jar
+cp module-src/mapdb-migrator/target/migrate.* modules/
+cp module-src/parties-module/target/parties* modules/
+cp module-src/rinaorc-legacy/target/rinaorc-legacy-users.jar modules/
+cp module-src/skywars-module/target/skywars-hook.jar modules/
