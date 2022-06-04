78e2f7e3e11199531b536ad0d323b0a1ac618ec7
Mats, Pc anouk <mats@ikeasupport.com>
2022-06-05 00:22:43 +0200

    Windows unit test patches

--- module-src/vistas-server/src/test/java/vistas/test/utils/ClassScanner.java
+++ module-src/vistas-server/src/test/java/vistas/test/utils/ClassScanner.java
@@ -3,6 +3,7 @@ package vistas.test.utils;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -26,7 +27,7 @@ public class ClassScanner {
         List<File> dirs = new ArrayList<File>();
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
+            dirs.add(new File(java.net.URLDecoder.decode(resource.getFile(), StandardCharsets.UTF_8.name())));
         }
         ArrayList<Class> classes = new ArrayList<Class>();
         for (File directory : dirs) {
