e66b005ca46e2184638afb69886f6a2662b78906
Mats <mats@craftmend.com>
2022-09-12 09:34:23 +0200

    Toggle moderation status and log it to users

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/voice/VoiceModSubCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/voice/VoiceModSubCommand.java
@@ -1,6 +1,10 @@
 package com.craftmend.openaudiomc.spigot.modules.commands.subcommands.voice;
 
+import com.craftmend.openaudiomc.generic.client.objects.ClientConnection;
 import com.craftmend.openaudiomc.generic.commands.interfaces.SubCommand;
+import com.craftmend.openaudiomc.generic.logging.OpenAudioLogger;
+import com.craftmend.openaudiomc.generic.networking.interfaces.NetworkingService;
+import com.craftmend.openaudiomc.generic.storage.enums.StorageKey;
 import com.craftmend.openaudiomc.generic.user.User;
 import org.bukkit.entity.Player;
 
@@ -18,7 +22,30 @@ public class VoiceModSubCommand extends SubCommand {
             return;
         }
 
+        if (!StorageKey.SETTINGS_VC_MOD_ENABLED.getBoolean()) {
+            message(sender, "Moderation is disabled in the config for security");
+            return;
+        }
 
+        // toggle mod status
+        ClientConnection client = (ClientConnection) sender.findClient().get();
+        client.getSession().setModerating(!client.getSession().isModerating());
+
+        if (client.getSession().isModerating()) {
+            OpenAudioLogger.toConsole(sender.getName() + " started moderating");
+            bcToStaff(sender.getName() + " started moderating");
+        } else {
+            OpenAudioLogger.toConsole(sender.getName() + " stopped moderating");
+            bcToStaff(sender.getName() + " stopped moderating");
+        }
+    }
+
+    private void bcToStaff(String string) {
+        for (ClientConnection client : getService(NetworkingService.class).getClients()) {
+            if (isAllowed(client.getUser())) {
+                message(client.getUser(), string);
+            }
+        }
     }
 
 }
