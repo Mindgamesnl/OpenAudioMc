8f90f9b707180cbf56f80ec3e8da6c85ac716555
Mats <mats@craftmend.com>
2022-10-13 13:32:19 +0200

    Test show serializer

--- plugin/src/test/java/com/craftmend/tests/TestShows.java
+++ plugin/src/test/java/com/craftmend/tests/TestShows.java
@@ -4,8 +4,11 @@ import com.craftmend.openaudiomc.OpenAudioMc;
 import com.craftmend.openaudiomc.generic.client.ClientDataService;
 import com.craftmend.openaudiomc.generic.client.enums.DataStoreVersion;
 import com.craftmend.openaudiomc.generic.client.store.ClientDataStore;
+import com.craftmend.openaudiomc.spigot.modules.show.ShowService;
 import com.craftmend.openaudiomc.spigot.modules.show.interfaces.ShowRunnable;
 import com.craftmend.openaudiomc.spigot.modules.show.objects.Show;
+import com.craftmend.openaudiomc.spigot.modules.show.objects.ShowCue;
+import com.craftmend.openaudiomc.spigot.modules.show.runnables.ChatRunnable;
 import com.craftmend.openaudiomc.spigot.modules.show.util.TimeParser;
 import com.craftmend.tests.helpers.TestHelper;
 import com.craftmend.utils.Waiter;
@@ -24,7 +27,7 @@ public class TestShows extends TestHelper {
     @SneakyThrows
     @BeforeClass
     public static void doYourOneTimeSetup() {
-        prepTests(true);
+        prepTests(false);
     }
 
     @Test
@@ -50,6 +53,30 @@ public class TestShows extends TestHelper {
         Assert.assertTrue(secondsSince(b) < 3.1);
         Assert.assertTrue(secondsSince(c) < 2.1);
         Assert.assertTrue(secondsSince(d) < 1.1);
+    }
+
+    @Test
+    public void testShowSaving() {
+        startQuietly();
+        String showName = "test-show-" + UUID.randomUUID().toString();
+
+        Show before = new Show(showName);
+
+
+        before.addCue(OpenAudioMc.getService(ShowService.class).createRunnable("chat", "helloworld", null), TimeParser.toMilis("1s"));
+        before.addCue(OpenAudioMc.getService(ShowService.class).createRunnable("chat", "helloworldtwo", null), TimeParser.toMilis("2s"));
+
+        String serializedShow = before.toString();
+
+        // load
+        Show after = OpenAudioMc.getService(ShowService.class).fromJson(serializedShow);
+        Assert.assertEquals(before.getShowName(), after.getShowName());
+
+        for (ShowCue showCue : before.getCueList()) {
+            Assert.assertNotNull(after.getCueById(showCue.getId()));
+        }
+
+        shutdown();
     }
 
     public double secondsSince(CallbackRunnable s) {
