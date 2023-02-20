17a2ee075320036783dbee758043e81359903e3f
Mats <mats@craftmend.com>
2023-02-20 19:26:58 +0100

    Remove client debug stuff

--- react_client/src/views/client/ClientView.js
+++ react_client/src/views/client/ClientView.js
@@ -9,38 +9,8 @@ import {GrayoutPage} from "../../components/layout/GrayoutPage";
 import {connect} from "react-redux";
 import {StaticFooter} from "../../components/footer/StaticFooter";
 import {InputModal} from "../../components/modal/InputModal";
-import {setGlobalState} from "../../state/store";
-import {reportVital} from "../../client/util/vitalreporter";
-import {toast} from "react-toastify";
-import {VoiceModule} from "../../client/services/voice/VoiceModule";
 
 class ClientView extends React.Component {
-
-    submitFeedback = () => {
-        setGlobalState({
-            inputModal: {
-                visible: true,
-                title: 'Feedback',
-                message: 'Please enter your feedback or bug report below:',
-                callback: function (message) {
-                    if (message) {
-                        reportVital("metrics:feedback | " + message + " | " + JSON.stringify(VoiceModule.peerManager.gatherDebugState()))
-                        toast.success('❤️ Thank you for your feedback!', {
-                            position: "top-center",
-                            autoClose: 5000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "dark",
-                        });
-                    }
-                }
-            }
-        })
-    }
-
     render() {
         let {title, message, footer} = this.props.loadingOverlay;
 
@@ -62,12 +32,6 @@ class ClientView extends React.Component {
                     />
                 </GrayoutPage>}
                 <ResetLanguageBanner/>
-
-                <StaticFooter onClick={this.submitFeedback}>
-                    <p className={"cursor-pointer"}>
-                        Public beta of our new client. Click here to report a bug or submit feedback.
-                    </p>
-                </StaticFooter>
 
                 {this.props.fixedFooter && <StaticFooter>{this.props.fixedFooter}</StaticFooter>}
                 {this.props.inputModal.visible && <InputModal />}
