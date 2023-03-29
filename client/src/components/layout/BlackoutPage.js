db2c66283708b6a6adfb20c2a48ee5cdba91ddf4
Mats <mats@craftmend.com>
2023-03-29 18:22:15 +0200

    Move login to the account style

--- client/src/components/layout/BlackoutPage.js
+++ client/src/components/layout/BlackoutPage.js
@@ -3,7 +3,7 @@ import React from "react";
 export class BlackoutPage extends React.Component {
     render() {
         return (
-            <div className="flex flex-col space-y-4 min-w-screen h-screen animated fadeIn faster fixed  left-0 top-0 flex justify-center items-center inset-0 z-40 outline-none focus:outline-none bg-gray-900">
+            <div className="flex flex-col space-y-4 min-w-screen h-screen animated fadeIn faster fixed  left-0 top-0 flex justify-center items-center inset-0 z-40 outline-none focus:outline-none" style={{background: "#242424"}}>
                 {this.props.children}
             </div>
         );
