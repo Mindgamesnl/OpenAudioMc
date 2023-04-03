5710bbc0371f863868e654eb71a2e9e6d6ccf7c6
Mats <mats@craftmend.com>
2023-04-03 12:50:58 +0200

    Scaling and independant rendering

--- client/src/views/client/pages/debug/DebugPage.js
+++ client/src/views/client/pages/debug/DebugPage.js
@@ -14,7 +14,8 @@ export default class DebugPage extends React.Component {
         super(props);
 
         this.state = {
-            task: null,
+            radarTask: null,
+            graphTask: null,
             clearColors: [
                 "Crimson",
                 "Navy",
@@ -28,12 +29,13 @@ export default class DebugPage extends React.Component {
             ],
             playerLocation: {},
             peers: [],
+            panels: []
         }
     }
 
     componentDidMount() {
         this.setState({
-            task: setInterval(() => {
+            radarTask: setInterval(() => {
                 this.setState({
                     playerLocation: {x: WorldModule.player.location.x,
                         y: WorldModule.player.location.y,
@@ -42,40 +44,46 @@ export default class DebugPage extends React.Component {
                     },
                     peers: VoiceModule.getPeerLocations()
                 })
-            }, 50)
+            }, 50),
+            graphTask: setInterval(() => {
+                let panels = []
+                let values = getDebugValues();
+
+                // sort values by name
+                let keys = Object.keys(values);
+                keys.sort();
+                let sortedValues = {};
+                for (let key of keys) {
+                    sortedValues[key] = values[key];
+                }
+                values = sortedValues;
+
+                let ci = 0;
+                for (let debugValueName in values) {
+                    // feed latest value to increment time
+                    let stat = values[debugValueName];
+                    let latest = getLatestDebugValue(stat);
+                    if (latest == null) latest = 0;
+                    panels.push(<DebugPanel title={debugValueName} color={this.state.clearColors[ci]} fill={stat.fill} data={stat.values} catchLine={"Latest:" + latest} key={debugValueName}/>)
+                    if (!stat.advancesItself) {
+                        feedDebugValue(stat, latest); // advance time
+                    }
+                    ci++;
+                    if (ci >= this.state.clearColors.length) ci = 0;
+                }
+
+                this.setState({
+                    panels: panels
+                })
+            }, 500)
         })
     }
 
     componentWillUnmount() {
-        clearInterval(this.state.task);
+        clearInterval(this.state.radarTask);
+        clearInterval(this.state.graphTask);
     }
     render() {
-        let panels = []
-        let values = getDebugValues();
-
-        // sort values by name
-        let keys = Object.keys(values);
-        keys.sort();
-        let sortedValues = {};
-        for (let key of keys) {
-            sortedValues[key] = values[key];
-        }
-        values = sortedValues;
-
-        let ci = 0;
-        for (let debugValueName in values) {
-            // feed latest value to increment time
-            let stat = values[debugValueName];
-            let latest = getLatestDebugValue(stat);
-            if (latest == null) latest = 0;
-            panels.push(<DebugPanel title={debugValueName} color={this.state.clearColors[ci]} fill={stat.fill} data={stat.values} catchLine={"Latest:" + latest} key={debugValueName}/>)
-            if (!stat.advancesItself) {
-                feedDebugValue(stat, latest); // advance time
-            }
-            ci++;
-            if (ci >= this.state.clearColors.length) ci = 0;
-        }
-
         return (
             <div className={"w-full h-full flex flex-col"}>
                 <div className="flex flex-wrap">
@@ -87,7 +95,7 @@ export default class DebugPage extends React.Component {
                         <p className={"text-white"}><b>Build tag:</b> <i className={"text-blue-200"}>{VERSION.tag}</i></p>
                         <p className={"text-white"}><b>Player:</b> <i className={"text-blue-200"}>{getGlobalState().currentUser.userName}</i></p>
                     </OaStyleCard>
-                    {panels}
+                    {this.state.panels}
 
 
                     <OaStyleCard title={"Radar"}>
