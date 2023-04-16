e4418ea0c31c282db62ea1e99c17ee743c8e001b
Mats <mats@craftmend.com>
2023-04-16 14:20:33 +0200

    Slight change

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -37,7 +37,7 @@ const initialState = {
         voicechatMuted: false,
         voicechatSurroundSound: true,
         voicechatMonitoringEnabled: false,
-        microphoneSensitivity: 90,
+        microphoneSensitivity: 75,
         automaticSensitivity: false,
         fadeAudio: true,
 
