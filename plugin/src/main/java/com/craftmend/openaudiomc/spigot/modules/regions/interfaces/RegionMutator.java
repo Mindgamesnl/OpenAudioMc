f4663a833425943fe3a5d2cfaded85452f95e8a9
Mats <mats@craftmend.com>
2023-04-20 22:22:58 +0200

    Fix value name

---
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/regions/interfaces/RegionMutator.java
@@ -0,0 +1,2 @@
+package com.craftmend.openaudiomc.spigot.modules.regions.interfaces;public interface RegionMutator {
+}
---
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/regions/registry/WorldRegionManager.java
@@ -0,0 +1,2 @@
+package com.craftmend.openaudiomc.spigot.modules.regions.registry;public class WorldRegionManager {
+}
