04211ac6251da7758937b42862d1fc338d9356d5
Mats <mats@craftmend.com>
2023-04-24 20:20:07 +0200

    Add a bg

---
+++ dev-resources/spawn-test-spigot-legacy.sh
@@ -0,0 +1,25 @@
+export JAVA_HOME=`/usr/libexec/java_home -v 17`
+mkdir -p test-server-spigot/plugins/
+mkdir -p test-server-spigot/plugins/OpenAudioMc/
+echo "Building new OpenAudioMc jar without unit tests.."
+
+mvn clean install -Dmaven.test.skip=true
+
+rm test-server-spigot/plugins/openaudiomc-*
+cp plugin/target/openaudiomc-* test-server-spigot/plugins/
+cp dev-resources/bukkit.yml test-server-spigot/
+cp dev-resources/ops.json test-server-spigot/
+
+cd test-server-spigot
+echo "Cleaning server..."
+if [ ! -f ./spigot-1.19.4.jar ]; then
+    wget https://download.getbukkit.org/spigot/spigot-1.19.4.jar
+    echo "eula=true" > eula.txt
+    chmod +x spigot-1.19.4.jar
+fi
+
+#export OA_ENVIRONMENT="DEVELOPMENT"
+#export OA_DEBUG_URL="http://localhost:8000"
+
+echo "Starting server.."
+java -Xms3G -Xmx3G -jar spigot-1.19.4.jar nogui
