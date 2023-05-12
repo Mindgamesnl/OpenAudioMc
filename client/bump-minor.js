f8039f36f6280b9eb51f4215415c745fa68120d5
Mats <mats@craftmend.com>
2023-05-12 20:40:40 +0200

    Build number change

--- client/bump-minor.js
+++ client/bump-minor.js
@@ -3,8 +3,7 @@ console.log('Incrementing build number...');
 fs.readFile('src/metadata.json', function (err, content) {
     if (err) throw err;
     var metadata = JSON.parse(content);
-    metadata.buildMinor = metadata.buildMinor + 1;
-    metadata.buildRevision = 0;
+    metadata.buildRevision = metadata.buildRevision + 1;
     metadata.buildDate = new Date().toTimeString();
     metadata.build = `${metadata.buildMajor}.${metadata.buildMinor}.${metadata.buildRevision} ${metadata.buildTag}`;
     fs.writeFile('src/metadata.json', JSON.stringify(metadata), function (err) {
