c53162e9565bc3fb7a9e149d8792d486a4b25ca3
Mats <mats@craftmend.com>
2023-06-05 00:33:38 +0200

    Use a cache for module classloader resolving, saves opperations and class searches when running Vistas

--- dev-resources/bump-binaries.sh
+++ dev-resources/bump-binaries.sh
@@ -1,12 +1,12 @@
 [ ! -d "plugin/" ] && echo "Execute this script from the repo root" && exit 1
 
 # Build everything
-mvn clean install -Dmaven.test.skip=true
+mvn -T 4.5C clean install -Dmaven.test.skip=true
 
 # copy modules
-cp module-src/vistas-server/target/vistas-server.jar modules/vistas-server.jar
-cp module-src/vistas-client/target/vistas-client.jar modules/vistas-client.jar
-cp module-src/mapdb-migrator/target/migrate.* modules/
-cp module-src/parties-module/target/parties* modules/
-cp module-src/rinaorc-legacy/target/rinaorc-legacy-users.jar modules/
-cp module-src/skywars-module/target/skywars-hook.jar modules/
+cp modules/vistas-server/target/vistas-server.jar modules/vistas-server.jar
+cp modules/vistas-client/target/vistas-client.jar modules/vistas-client.jar
+cp modules/mapdb-migrator/target/migrate.* modules/
+cp modules/parties-module/target/parties* modules/
+cp modules/rinaorc-legacy/target/rinaorc-legacy-users.jar modules/
+cp modules/skywars-module/target/skywars-hook.jar modules/
