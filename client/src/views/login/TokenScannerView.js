f0fc42c6bc3b4dd05ec355cac4ffc2b46d0ec786
Mats <mats@ikeasupport.com>
2023-07-03 18:18:32 +0200

    And now to this

--- client/src/views/login/TokenScannerView.js
+++ client/src/views/login/TokenScannerView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {OaStyleCard} from "../../components/card/OaStyleCard";
 import {BlackoutPage} from "../../components/layout/BlackoutPage";
 import Webcam from 'react-webcam';
+import cv from '@techstark/opencv-js'
 
 export class TokenScannerView extends React.Component {
 
@@ -11,15 +12,35 @@ export class TokenScannerView extends React.Component {
     }
 
     capture = () => {
+        alert("Capturing")
         const imageSrc = this.webcamRef.current.getScreenshot();
-        // Handle the captured image as needed
-    }
+        const imageElement = document.createElement('img');
 
-  render() {
+        imageElement.onload = () => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = imageElement.width;
+            canvas.height = imageElement.height;
+            ctx.drawImage(imageElement, 0, 0, imageElement.width, imageElement.height);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const srcMat = cv.matFromImageData(imageData);
+
+            // Perform image processing tasks using OpenCV.js
+
+            srcMat.delete(); // Clean up the memory
+        };
+
+        imageElement.src = imageSrc;
+    };
+
+
+    render() {
     return (
         <BlackoutPage coverImage={"assets/login-background.png"}>
           <div className="py-12">
             <OaStyleCard title={"Scan login code"} isDanger={false} fullWidth={true}>
+                <img ref={this.imageRef} alt={"Camera"} style={{width: "100%"}}/>
                 <Webcam
                     audio={false}
                     videoConstraints={{
