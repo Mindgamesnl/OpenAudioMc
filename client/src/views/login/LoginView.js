bc17d341e1e9650e39d47203cd6ea6c9e3b8dfa4
Mats <mats@craftmend.com>
2023-07-06 21:19:16 +0200

    Add docs link

--- client/src/views/login/LoginView.js
+++ client/src/views/login/LoginView.js
@@ -36,10 +36,26 @@ export class LoginView extends React.Component {
                 <div className="py-12">
                     <OaStyleCard title={"Invalid session"} isDanger={true} body={<LoginForm/>} fullWidth={true}/>
                 </div>
+
+                <div className="relative block common-rounded-top pt-2 common-rounded-bottom shadow-lg p-6 themed-text clickprompt-box">
+                    <div className={"w-full"}>
+                        <div className={"text-white m-5"}>
+                            <div className="flex items-center justify-center align-middle space-x-3">
+                                More info about OpenAudioMc
+                            </div>
+                        </div>
+                        <a href={"https://openaudiomc.net/docs/client_guide"}
+                                className="w-full block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium common-rounded-top common-rounded-bottom text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Client Documentation
+                        </a>
+                    </div>
+                </div>
+
                 <StaticFooter>
                     <a href={"https://openaudiomc.net/docs/client_major_changelog"} className={"text-white"}>
                         build {VERSION.build}
-                        <small className={"pl-2 " + this.state.versionDiff.color}>({this.state.versionDiff.text})</small>
+                        <small
+                            className={"pl-2 " + this.state.versionDiff.color}>({this.state.versionDiff.text})</small>
                     </a>
                 </StaticFooter>
             </BlackoutPage>
