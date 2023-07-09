2503584a33eeffb774c1b032e640fdc4e27703b3
Mats <mats@craftmend.com>
2023-07-09 14:15:41 +0200

    Docker based bungee test setup

---
+++ dev-resources/spawn-test-bungee-docker.sh
@@ -0,0 +1,17 @@
+export JAVA_HOME=`/usr/libexec/java_home -v 17`
+mkdir -p test-server-spigot/plugins/
+mkdir -p test-server-spigot/plugins/OpenAudioMc/
+echo "Building new OpenAudioMc jar without unit tests.."
+
+cd plugin
+./src/main/bash/post-build.sh
+cd ..
+
+mvn -T 4.5C clean install -Dmaven.test.skip=true
+
+# permission workaround
+rm -rf dev-resources/bungee-test/plugins/
+mkdir -p dev-resources/bungee-test/plugins/
+cp plugin/target/openaudiomc-*.jar dev-resources/bungee-test/plugins/
+cd dev-resources/bungee-test/
+docker-compose up
