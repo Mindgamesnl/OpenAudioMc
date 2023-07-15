920d57eee56004c32d339f895f8d39ddeac4315e
YaoSiQian <2229561981@qq.com>
2023-07-15 20:12:15 +0800

    Translate SimpChinese

--- client/src/client/translations/MessageModule.js
+++ client/src/client/translations/MessageModule.js
@@ -22,6 +22,7 @@ export class MessageModule {
             "de": "de.lang", // german
             "ja": "jp.lang", // japanese
             "ko": "kr.lang", // korean
+            "zhcn": "zhcn.lang", // simplified chinese
 
             // One of these can probably go, but i wasn't provided
             // enough info and am unable to find the correct region code
