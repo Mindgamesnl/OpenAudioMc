1ba8773fc9029032e8c040d76b42872562c29e12
Mats <mats@craftmend.com>
2023-08-09 17:35:10 +0200

    Eslint in normal dev

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 import svgr from "vite-plugin-svgr";
+import eslint from 'vite-plugin-eslint';
+
 
 export default defineConfig({
     plugins: [
@@ -22,6 +23,7 @@ export default defineConfig({
             },
         },
         react(),
+        eslint()
     ],
     server: {
         port: 3000,
