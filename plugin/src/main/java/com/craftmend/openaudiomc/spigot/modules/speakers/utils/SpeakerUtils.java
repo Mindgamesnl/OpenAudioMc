f1b81a1abc2a90cf880f220e88f5d31222832e1e
Mats <mats@craftmend.com>
2023-10-07 12:44:18 +0200

    Implement proxy classes to fix speakers on 1.20.2

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/utils/SpeakerUtils.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/speakers/utils/SpeakerUtils.java
@@ -1,12 +1,15 @@
 package com.craftmend.openaudiomc.spigot.modules.speakers.utils;
 
 import com.craftmend.openaudiomc.OpenAudioMc;
+import com.craftmend.openaudiomc.generic.logging.OpenAudioLogger;
+import com.craftmend.openaudiomc.generic.utils.ClassMocker;
 import com.craftmend.openaudiomc.spigot.OpenAudioMcSpigot;
 import com.craftmend.openaudiomc.spigot.modules.speakers.SpeakerService;
 import com.craftmend.openaudiomc.spigot.services.server.enums.ServerVersion;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.block.Skull;
 import org.bukkit.inventory.ItemStack;
@@ -17,25 +20,33 @@ import java.util.UUID;
 
 public class SpeakerUtils {
 
-    public static final String speakerSkin = "OpenAudioMC";
+    public static final String speakerSkin = "OpenAudioMc";
     public static final UUID speakerUUID = UUID.fromString("c0db149e-d498-4a16-8e35-93d57577589f");
     private static final SpeakerService SPEAKER_SERVICE = OpenAudioMc.getService(SpeakerService.class);
 
+    private static OfflinePlayer proxiedPlayer;
+
     static {
-        //Initialize the offline player one time before using it in the plugin. This caches the skin if it's not present
-        //so when someone wants to get a head it actually shows the skin and not a Steve head
-        Bukkit.getOfflinePlayer(speakerUUID);
+        // attempt to create a offline player instance
+        try {
+            proxiedPlayer = new ClassMocker<OfflinePlayer>(OfflinePlayer.class)
+                    .addReturnValue("getUniqueId", speakerUUID)
+                    .addReturnValue("getName", speakerSkin)
+                    .createProxy();
+        } catch (Exception e) {
+            OpenAudioLogger.toConsole("Failed to create a OfflinePlayer proxy class. This will cause issues with speakers on servers running post 1.20.2");
+        }
     }
 
     public static boolean isSpeakerSkull(Block block) {
         if (block.getState() instanceof Skull) {
             Skull skull = (Skull) block.getState();
             if (SPEAKER_SERVICE.getVersion() == ServerVersion.MODERN) {
-                if(skull.getOwningPlayer() == null) {
+                if (skull.getOwningPlayer() == null) {
                     if (skull.getOwner() == null) return false;
 
                     boolean valid = skull.getOwner().equalsIgnoreCase(speakerSkin);
-                    if(valid) {
+                    if (valid) {
                         //Possible edge case when someone upgrades the server from 1.11 to 1.12 or higher
                         skull.setOwningPlayer(Bukkit.getOfflinePlayer(speakerUUID));
                     }
@@ -55,12 +66,16 @@ public class SpeakerUtils {
         ItemStack skull = new ItemStack(SPEAKER_SERVICE.getPlayerSkullItem());
         skull.setDurability((short) 3);
         SkullMeta sm = (SkullMeta) skull.getItemMeta();
-        if(sm != null) {
-            if(SPEAKER_SERVICE.getVersion() == ServerVersion.MODERN) {
-                sm.setOwningPlayer(Bukkit.getOfflinePlayer(speakerUUID));
-            } else {
-                sm.setOwner(speakerSkin);
+        if (sm != null) {
+            if (SPEAKER_SERVICE.getVersion() == ServerVersion.MODERN) {
+                if (proxiedPlayer != null) {
+                    sm.setOwningPlayer(proxiedPlayer);
+                } else {
+                    // fallback for servers that don't support the proxy class
+                    sm.setOwningPlayer(Bukkit.getOfflinePlayer(speakerUUID));
+                }
             }
+
             sm.setOwner(speakerSkin);
             sm.setDisplayName(ChatColor.AQUA + "OpenAudioMc Speaker");
             sm.setLore(Arrays.asList(
@@ -70,7 +85,7 @@ public class SpeakerUtils {
                     "",
                     ChatColor.AQUA + "SRC: " + ChatColor.GREEN + source,
                     ChatColor.AQUA + "Radius: " + ChatColor.GREEN + radius
-                    ));
+            ));
             skull.setItemMeta(sm);
         }
 
