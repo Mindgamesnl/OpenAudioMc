33fb2699eb667d9b4fe9abcabdb01953162d2a00
Mats <mats@craftmend.com>
2023-12-09 23:58:38 +0100

    rls

--- client/src/client/util/vitalreporter.jsx
+++ client/src/client/util/vitalreporter.jsx
@@ -3,6 +3,8 @@ import { VERSION } from '../../build';
 import { getTranslation } from '../OpenAudioAppContainer';
 import { API_ENDPOINT } from '../config/ApiEndpoints';
 
+const lastErrorTimestamp = 0;
+
 export async function reportVital(message) {
   const u = getGlobalState().currentUser;
   const { userName, uuid } = u || { userName: 'unknown', uuid: 'unknown' };
@@ -12,6 +14,9 @@ export async function reportVital(message) {
   const currentDomain = window.location.hostname;
 
   message += ` | ${currentDomain} | ${VERSION.build} | ${serverName}`;
+
+  // cancel if the last error was less than 30 seconds ago
+  if (Date.now() - lastErrorTimestamp < 30000) return;
 
   await fetch(API_ENDPOINT.VITALS, {
     method: 'POST',
