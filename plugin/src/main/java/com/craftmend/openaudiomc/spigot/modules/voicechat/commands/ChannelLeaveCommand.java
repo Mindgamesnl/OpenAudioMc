73e03566f5c14249c9565c08984345428f547577
Mats <mats@craftmend.com>
2024-02-04 00:57:08 +0100

    Leave

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/commands/ChannelLeaveCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/voicechat/commands/ChannelLeaveCommand.java
@@ -1,7 +1,13 @@
 package com.craftmend.openaudiomc.spigot.modules.voicechat.commands;
 
+import com.craftmend.openaudiomc.api.clients.Client;
+import com.craftmend.openaudiomc.generic.client.objects.ClientConnection;
 import com.craftmend.openaudiomc.generic.commands.interfaces.SubCommand;
+import com.craftmend.openaudiomc.generic.commands.objects.CommandError;
 import com.craftmend.openaudiomc.generic.user.User;
+import com.craftmend.openaudiomc.spigot.modules.voicechat.VoiceChannelService;
+import com.craftmend.openaudiomc.spigot.modules.voicechat.channels.Channel;
+import lombok.SneakyThrows;
 
 public class ChannelLeaveCommand extends SubCommand {
 
@@ -10,7 +16,25 @@ public class ChannelLeaveCommand extends SubCommand {
     }
 
     @Override
+    @SneakyThrows
     public void onExecute(User sender, String[] args) {
+        if (!sender.findClient().isPresent()) {
+            throw new CommandError("Only players can create channels");
+        }
 
+        Client client = (Client) sender.findClient().get();
+        if (!client.hasVoicechatEnabled()) {
+            throw new CommandError("You must first have voice chat enabled before you can create a channel");
+        }
+
+        ClientConnection clientConnection = (ClientConnection) client;
+        Channel channel = clientConnection.getRtcSessionManager().getCurrentChannel();
+
+        if (channel == null) {
+            throw new CommandError("You are not in a channel");
+        }
+
+        channel.removeMember(sender);
+        message(sender, "You have left the channel");
     }
 }
