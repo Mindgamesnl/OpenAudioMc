05a3182c9d8475384e0dc594caf4ec3e493ddfde
Mats <mats@craftmend.com>
2024-02-11 15:10:25 +0100

    Javadoc for packages

---
+++ api/src/main/java/com/craftmend/openaudiomc/api/basic/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains some core abstractions and interfaces that are used in the OpenAudioMc API, mostly to wrap platform native components.
+ */
+package com.craftmend.openaudiomc.api.basic;
