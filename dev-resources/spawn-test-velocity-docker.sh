4047b1bf5a61586566f1ba3af57858e1f80fa263
Mats <mats@craftmend.com>
2024-05-01 22:05:07 +0200

    Velocity dev env

--- dev-resources/spawn-test-velocity-docker.sh
+++ dev-resources/spawn-test-velocity-docker.sh
@@ -10,8 +10,8 @@ cd ..
 mvn -T 4.5C clean install -Dmaven.test.skip=true
 
 # permission workaround
-rm -rf dev-resources/bungee-test/plugins/
-mkdir -p dev-resources/bungee-test/plugins/
-cp plugin/target/openaudiomc-*.jar dev-resources/bungee-test/plugins/
-cd dev-resources/bungee-test/
+rm -rf dev-resources/velocity-test/plugins/openaudiomc-*.jar
+mkdir -p dev-resources/velocity-test/plugins/
+cp plugin/target/openaudiomc-*.jar dev-resources/velocity-test/plugins/
+cd dev-resources/velocity-test/
 docker-compose up --build
