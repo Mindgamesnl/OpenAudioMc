5bbc2f68c54d9519fefb5a966d6b31fea8673fb3
Mats Auraot <10709682+Mindgamesnl@users.noreply.github.com>
2024-08-02 03:43:51 +0200

    fix readability

--- client/src/components/voice/ui/GeneralVoiceSettings.jsx
+++ client/src/components/voice/ui/GeneralVoiceSettings.jsx
@@ -129,7 +129,7 @@ class GeneralVoiceSettings extends React.Component {
               <div
                 className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"
               />
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              <span className="ms-3 text-sm font-medium">
                 {msg('vc.settings.completelyFadePeers')}
               </span>
             </label>
@@ -146,9 +146,7 @@ class GeneralVoiceSettings extends React.Component {
               <div
                 className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"
               />
-              <span
-                className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
+              <span className="ms-3 text-sm font-medium">
                 {msg('vc.settings.monitoring.title')}
               </span>
             </label>
@@ -165,7 +163,7 @@ class GeneralVoiceSettings extends React.Component {
               <div
                 className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"
               />
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              <span className="ms-3 text-sm font-medium">
                 {msg('vc.settings.surround.toggle')}
               </span>
             </label>
@@ -182,7 +180,7 @@ class GeneralVoiceSettings extends React.Component {
               <div
                 className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"
               />
-              <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              <span className="ms-3 text-sm font-medium">
                 {msg('vc.automaticAdjustments')}
               </span>
             </label>
