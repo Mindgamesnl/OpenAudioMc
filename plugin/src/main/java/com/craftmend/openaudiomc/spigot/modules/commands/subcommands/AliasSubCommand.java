7622344a4c4ad7ed308f6c06c11cbeeea7464979
Mats <mats@craftmend.com>
2024-11-22 18:23:24 +0100

    hell ye

--- plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/AliasSubCommand.java
+++ plugin/src/main/java/com/craftmend/openaudiomc/spigot/modules/commands/subcommands/AliasSubCommand.java
@@ -22,23 +22,20 @@ public class AliasSubCommand extends SubCommand {
                 new Argument("<name> <source>",
                         "Register a Alias for a source URL so you can easaly memorize them and can paste them onto signs without having to type a complete dictionary." +
                                 " When an alias like onride_music is set, you can trigger it by using a:onride_music as your source.")
-                        .addTabCompleteProvider(1, (sender) -> new String[]{"<alias-name>"})
-                        .addTabCompleteProvider(2, MediaTabcompleteProvider.getInstance()),
+                        .addTabCompleteProvider(0, (sender) -> new String[]{"<name>"})
+                        .addTabCompleteProvider(1, MediaTabcompleteProvider.getInstance()),
 
                 new Argument("delete <name>",
                         "Delete a alias from the database. This will also remove it from the memory cache.")
-                        .addTabCompleteProvider(1, (sender) -> new String[]{"<alias-name>"})
-                        .addTabCompleteProvider(2, AliasNameTabCompleter.getInstance()),
+                        .addTabCompleteProvider(1, AliasNameTabCompleter.getInstance()),
 
                 new Argument("resolve <name>", "Resolve a alias to see what it's target is.")
-                        .addTabCompleteProvider(1, (sender) -> new String[]{"<alias-name>"})
-                        .addTabCompleteProvider(2, AliasNameTabCompleter.getInstance())
+                        .addTabCompleteProvider(1, AliasNameTabCompleter.getInstance())
         );
     }
 
     @Override
     public void onExecute(User sender, String[] args) {
-        sender.sendMessage("Length: " + args.length);
         if (args.length == 2 && !args[0].equalsIgnoreCase("delete") && !args[0].equalsIgnoreCase("resolve")) {
             String aliasName = args[0].toLowerCase();
 
@@ -51,7 +48,7 @@ public class AliasSubCommand extends SubCommand {
 
             message(sender, ChatColor.GREEN + "Success! the alias " + ChatColor.YELLOW + "a:" + aliasName.toLowerCase() + ChatColor.GRAY + " will be read as " + ChatColor.YELLOW + aliasSource);
             return;
-        } else if (args.length == 1) {
+        } else if (args.length == 2) {
             String command = args[0].toLowerCase();
 
             switch (command) {
@@ -66,7 +63,7 @@ public class AliasSubCommand extends SubCommand {
                     } else {
                         message(sender, ChatColor.RED + "Error! the alias " + ChatColor.YELLOW + "a:" + args[1].toLowerCase() + ChatColor.GRAY + " does not exist.");
                     }
-                    break;
+                    return;
 
                 case "resolve":
                     Alias resolved = OpenAudioMc.getService(AliasService.class).getAliasMap().get(args[1].toLowerCase(Locale.ROOT));
@@ -75,7 +72,7 @@ public class AliasSubCommand extends SubCommand {
                     } else {
                         message(sender, ChatColor.RED + "Error! the alias " + ChatColor.YELLOW + "a:" + args[1].toLowerCase() + ChatColor.GRAY + " does not exist.");
                     }
-                    break;
+                    return;
             }
         }
 
